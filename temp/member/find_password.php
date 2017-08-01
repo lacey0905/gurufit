9804bd018c04c7695c54d5a5f3b07320f79ec132
user <user@LAPTOP-5K4FIEFV>
2017-08-01 18:28:59 +0900

    0801

---
+++ temp/member/find_password.php
@@ -0,0 +1,59 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<article class="sub_content custom_inner">
+		<section class="sub_head">
+			<h2>Serch Email / Password</h2>
+			<div class="tab_active_type">
+				<ul>
+					<li class="active"><a href="#">아이디찾기</a></li>
+					<li><a href="#">비밀번호찾기</a></li>
+				</ul>
+			</div>
+		</section>
+		<div class="member_box">
+			<h3>
+				비밀번호 찾기
+				<em>이메일로 새 비밀번호를 설정할 수 있는 링크를 발송합니다.</em>
+			</h3>
+			<div id="userSearchBox" class="box_inner">
+				<div class="inp_login">
+					<form action="" method="">
+						<fieldset>
+							<legend class="blind">비밀번호 찾기</legend>
+							<input type="text" value="" placeholder="이름" class="inp_nm_box inp_full" name="name">
+							<input type="password" value="" placeholder="이메일주소" class="inp_nm_box inp_full" name="email">
+							<input type="submit" value="비밀번호 링크 발송" class="inp_submit_a inp_full">
+							<div class="custom_linkBox">
+								<a href="#">로그인으로 돌아가기</a>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</div>
+	</article>
+</section>
+<?php include "../inc/footer.php" ?>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ temp/product/store_list.php
@@ -0,0 +1,135 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<article class="sub_content custom_inner">
+		<section class="sub_head">
+			<h2 class="s_lar">Store</h2>
+			<div class="tab_active_type">
+				<ul>
+					<li class="active"><a href="#">스포츠 (179)</a></li>
+					<li><a href="#">스니커즈 (179)</a></li>
+					<li><a href="#">옥스포드 (179)</a></li>
+					<li><a href="#">로퍼 (179)</a></li>
+					<li><a href="#">부츠 (179)</a></li>
+					<li><a href="#">슬립온 (179)</a></li>
+					<li><a href="#">샌들 (179)</a></li>
+				</ul>
+			</div>
+			<dl class="search_result result_tag">
+				<dt>총 <strong>3,517</strong>개의 검색 결과가 있습니다.</dt>
+				<dd class="tag_bg_a"><a href="#">KimHayoung</a></dd>
+				<dd class="tag_bg_b"><a href="#">주황</a></dd>
+				<dd class="tag_bg_b"><a href="#">Nike</a></dd>
+				<dd class="tag_bg_b"><a href="#">스포츠</a></dd>
+			</dl>
+		</section>
+		<?php include "../inc/prd_side.php" ?>
+		<section class="content_area">
+			<dl class="select_sort">
+				<dt>정렬 기준</dt>
+				<dd>
+					<div class="sel_custom sel_nm_box">
+						<select class="sel_mid">
+							<option>추천 제품 순</option>
+						</select>
+					</div>
+				</dd>
+			</dl>
+			<!-- 
+			<script>
+				$(function(){
+					var temp;
+					for(var i=0; i<5; i++){
+						temp = $(".prd_type_b > ul > li").clone();
+						$(".prd_type_b > ul").append(temp);
+					}
+				});
+			</script>
+			 -->
+			<article class="prd_type_b">
+				<ul class="x4 opt_pop">
+					<li class="">
+						<figure>
+	    					<span class="thum">
+	    						<a href="#" class="my_list_btn"><i class="fa fa-heart-o" aria-hidden="true"></i>나의 리스트에 담기</a>
+	    						<a href="http://lacey.dothome.co.kr/yeseo/gurufit/product/store_detail.php">
+	    							<img src="../img/store_sample_230x160.png" alt="샘플상품">
+    							</a>
+   							</span>
+	    					<div class="rating">
+	    						<strong class="star_rating">
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_off"></i>
+	   							</strong>
+	    						<span class="cnt">(29)</span>
+	    						<span class="sale">Sale 40%</span>
+	    					</div>
+	    					<figcaption>
+	    						<span class="subject">NIKE FREE 5.0</span>
+	    						<span class="price">
+	    							<strong class="sell_price">89,100<em>원</em></strong>
+	    							<del class="customer_price">99,000원</del>
+	    						</span>
+	    					</figcaption>
+	    					<div class="open_detail">
+    							<div class="detial_color_list">
+    								<ul>
+    									<li class="c_black">Black</li>
+    									<li class="c_blue">Blue</li>
+    									<li class="c_red">Red</li>
+    									<li class="c_yellow">Yellow</li>
+    									<li class="c_white">White</li>
+    								</ul>
+    							</div>
+    							<div class="detial_opt_list">
+									<ul class="check_opt_box">
+										<li class="active"><a href="#">230</a></li>
+										<li><a href="#">235</a></li>
+										<li><a href="#">240</a></li>
+										<li><a href="#">245</a></li>
+										<li><a href="#">250</a></li>
+										<li><a href="#">255</a></li>
+										<li><a href="#">260</a></li>
+										<li class="active"><a href="#">265</a></li>
+										<li><a href="#">270</a></li>
+										<li><a href="#">275</a></li>
+										<li><a href="#">280</a></li>
+										<li><a href="#">285</a></li>
+										<li class="active"><a href="#">290</a></li>
+										<li><a href="#">295</a></li>
+										<li><a href="#">300</a></li>
+										<li><a href="#">305</a></li>
+									</ul>
+    							</div>
+    						</div>
+						</figure>
+					</li>
+				</ul>
+			</article>
+		</section>
+	</article>
+</section>
+<?php include "../inc/footer.php" ?>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 

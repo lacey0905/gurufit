ae578afeb27c5f7e119cec450e8d32f92e92c798
user <user@LAPTOP-5K4FIEFV>
2017-08-04 18:22:11 +0900

    0804

---
+++ temp/mypage/dash_board.php
@@ -0,0 +1,41 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<article class="sub_content custom_inner">
+		<section class="sub_head">
+			<h2>My Page</h2>
+			<div class="tab_active_type">
+				<ul>
+					<li class="active"><a href="#">대시보드</a></li>
+					<li><a href="#">멤버십</a></li>
+					<li><a href="#">알림</a></li>
+					<li><a href="#">새로운 활동</a></li>
+					<li><a href="#">나의 리스트</a></li>
+					<li><a href="#">히스토리</a></li>
+					<li><a href="#">1:1문의</a></li>
+					<li><a href="#">개인정보 설정</a></li>
+					<li><a href="#">구매내역</a></li>										
+				</ul>
+			</div>
+			
+			
+			<div class="my_dash_board">
+				<div class="dash_x3">
+					<h3 class="dash_tit">공개 뱃지</h3>
+				</div>
+			</div>
+			
+			
+		</section>
+	</article>
+</section>
+<?php include "../inc/footer.php" ?>
+ 

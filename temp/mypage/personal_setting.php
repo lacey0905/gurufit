d334ef8d2976c0e4b9cd62085e717d2e31cb48fe
yeseo <yeseo@shoe4rabbit.com>
2017-08-04 18:34:50 +0900

    개인정보 설정

--- temp/mypage/personal_setting.php
+++ temp/mypage/personal_setting.php
@@ -33,154 +33,113 @@
 			<article class="personal_setting_wrapper">
 				<table>
 					<tr class="login_info">
-						<td>로그인 정보</td>
+						<th>로그인 정보</th>
 						<td>
-							<ul>
-								<li>이메일 주소</li>
-								<li>비밀번호</li>
-							</ul>
-						</td>
-						<td>
-							<ul>
-								<li>groovy@shoeforrabbit.com<a href="#"><span>이메일 주소 변경</span></a></li>
-								<li><a href="#"><span>비밀번호 변경</span></a></li>
-							</ul>
+							<dl>
+								<dt>이메일 주소</dt>
+								<dd>groovy@shoeforrabbit.com<a href="#"><span class="email_change">이메일 주소 변경</span></a></dd>
+								<dt>비밀번호</dt>
+								<dd><a href="#"><span>비밀번호 변경</span></a></dd>
+							</dl>
 						</td>
 					</tr>
 					<tr class="personal_info">
-						<td>개인정보</td>
+						<th>개인정보</th>
 						<td>
-							<ul>
-								<li>이름</li>
-								<li>닉네임</li>
-								<li>휴대전화번호</li>
-								<li>생년월일</li>
-								<li>성별</li>
-							</ul>
-						</td>
-						<td>
-							<ul>
-								<li>홍길동</li>
-								<li>단호박정식</li>
-								<li>010-4321-1234<span class="setting_confirm"><i class="fa fa-check" aria-hidden="true"></i>인증됨</span></li>
-								<li>1980년 11월 15일</li>
-								<li>
+							<dl>
+								<dt>이름</dt>
+								<dd>홍길동</dd>
+								<dd>편집</dd>
+								<dt>닉네임</dt>
+								<dd>단호박정식</dd>
+								<dt>휴대전화번호</dt>
+								<dd>010-4321-1234<i class="fa fa-check" aria-hidden="true"></i></dd>
+								<dt>생년월일</dt>
+								<dd>1980년 11월 15일</dd>
+								<dt>성별</dt>
+								<dd>
 									<span>여성</span>
 									<span>남성</span>
-								</li>
-								<li class="setting_edit"><a href="#">편집</a></li>
-							</ul>
+								</dd>
+							</dl>
 						</td>
 					</tr>
 					<tr class="shopping_info">
-						<td>쇼핑정보</td>
+						<th>쇼핑정보</th>
 						<td>
-							<ul>
-								<li>배송지 주소</li>
-							</ul>
-						</td>
-						<td>
-							<table class="ship_address">
-								<tr>
-									<td>이름</td>
-									<td>김하영</td>
-									<td>연락처</td>
-									<td>051-6215-8168</td>
-									<td>편집</td>
-								</tr>
-								<tr>
-									<td>주소</td>
-									<td>47241</td>
-									<td>&nbsp;</td>
-									<td>&nbsp;</td>
-									<td>&nbsp;</td>
-								</tr>
-								<tr>
-									<td>&nbsp;</td>
-									<td colspan="4">부산광역시 부산진구 전포대로275번길 20, 서면롯데캐슬스카이 123-2345</td>
-								</tr>								
-							</table>
-							<table class="ship_address">
-								<tr>
-									<td>이름</td>
-									<td>김하영</td>
-									<td>연락처</td>
-									<td>051-6215-8168</td>
-									<td>편집</td>
-								</tr>
-								<tr>
-									<td>주소</td>
-									<td>47241</td>
-									<td>&nbsp;</td>
-									<td>&nbsp;</td>
-									<td>&nbsp;</td>
-								</tr>
-								<tr>
-									<td>&nbsp;</td>
-									<td colspan="4">부산광역시 부산진구 전포대로275번길 20, 서면롯데캐슬스카이 123-2345</td>
-								</tr>								
-							</table>
-							<ul>
-								<li class="setting_btn_big">
-									<a href="#">+ 새로운 주소 입력 </a>
-								</li>
-							</ul>							
+							<dl>
+								<dt>배송지주소</dt>
+								<dd>
+									<p>이름 <span>김하영</span></p>
+									<p>연락처 <span>051-6215-8168</span></p>
+									<p>편집</p>	
+								</dd>
+								<dd>
+									<p>주소<span>47241</span></p>
+									<p>부산광역시 부산진구 전포대로275번길 20, 서면롯데캐슬스카이 123-2345</p>
+								</dd>
+								<dd>
+									<span>+ 새로운 주소 입력</span>
+								</dd>
+							</dl>
 						</td>
 					</tr>
 					<tr class="personal_alarm_setting">
-						<td>알림설정</td>
+						<th>알림설정</th>
 						<td>
-							<ul>
-								<li>이메일 알림 설정</li>
-								<li>웹사이트 알림 설정</li>
-							</ul>
-						</td>
-						<td class="alram">
-							<table class="personal_email_alarm_setting">
-								<tr>
-									<td>
-										<h4>콘텐트 알림</h4>
-										<dl>
-											<dt>새로운 콘텐츠</dt>
-											<dd>팔로우 중인 guru/슈어드바이저가 새로운 콘텐츠 업데이트하면 이메일 알림을 보냅니다.</dd>
-											<dt>연관 콘텐츠</dt>
-											<dd>장바구니 및 마이 리스트에 담긴 상품/콘텐츠와 연관된 콘텐츠를 이메일로 보내드립니다.</dd>
-										</dl>
-										<h4>쇼핑 정보 알림</h4>
-										<dl>
-											<dt>연관 상품 업데이트 정보</dt>
-											<dd>
-											장바구니,컬렉션,마이 리스트에 담긴 상품에 변동사항이 있는 경우 이메일 알림을 보냅니다.<br/>
-											가격 변동,세일,품절,재입고 등
-											</dd>
-											<dt>이벤트 프로모션 정보</dt>
-											<dd>구루핏 쇼핑에서 진행하는 이벤트와 프로모션이 있는 경우 이메일 알림을 보내드립니다.</dd>
-										</dl>
-										<h4>활동 업데이트</h4>
-										<span>
-											<span></span>
-											<span></span>
-										</span>
-										<ul>
-											<li>경험치 알림<span>리뷰 입력,사진등록,소셜 활동을 통해 얻은 경험치 정보를 알려드립니다.</span></li>
-											<li>뱃지알림<span>활동을 통해 얻은 뱃지 정보를 알려드립니다.</span></li>
-											<li>공감알림<span>지정한 기간 동안 전에 등록한 리뷰와 사진이 얼마나 많은 공감을 얻었는지 알려드립니다.</span></li>
-										</ul>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<dl>
-											<dt>알림 갯수 표시</dt>
-											<dd>읽지 않은 웹사이트 알림 갯수를 표시합니다.</dd>
-										</dl>
-										<dl>
-											<dt>팝업 알림 표시</dt>
-											<dd>새로운 알림이 있는 경우 팝업으로 내용을 표시합니다.</dd>
-										</dl>
-									</td>
-								</tr>
-							</table>
+							<dl>
+								<dt>이메일 알림 설정</dt>
+								<dd>
+									<dl>
+										<dt>새로운 콘텐츠</dt>
+										<dd>팔로우 중인 guru / 슈어드바이저가 새로운 콘텐츠 업데이트하면 이메일 알림을 보냅니다.</dd>
+										<dt>연관 콘텐츠</dt>
+										<dd>장바구니 및 마이 리스트에 담긴 상품/콘텐츠와 연관된 콘텐츠를 이메일로 보내드립니다.</dd>
+									</dl>	
+								</dd>
+								<dd>
+									<dl>
+										<dt>쇼핑 정보 알림</dt>
+										<dd>
+											<dl>
+												<dt>연관 상품 업데이트 정보</dt>
+												<dd>
+												장바구니, 컬렉션, 마이 리스트에 담긴 상품에 변동사항이 있는 경우 이메일 알림을 보냅니다.<br>
+												가격 변동, 세일, 품절, 재입고 등<br>												
+												</dd>
+												<dt>이벤트 프로모션 정보</dt>
+												<dd>구루핏 쇼핑에서 진행하는 이벤트와 프로모션이 있는 경우 이메일 알림을 보내드립니다.</dd>
+											</dl>
+										</dd>
+										<dt>활동 업데이트</dt>
+										<dd>
+											<span>주간 업데이트</span>
+											<span>월간 업데이트</span>
+										</dd>
+										<dd>
+											<p><strong>경험치 알림</strong>리뷰 입력,사진등록,소셜활동을 통해 얻은 경험치 정보를 알려드립니다.</p>
+											<p><strong>뱃지알림</strong>활동을 통해 얻은 뱃지 정보를 알려드립니다.</p>
+											<p><strong>공감알림</strong>지정한 기간 동안 전에 등록한 리뷰와 사진이 얼마나 많은 공감을 얻었는지 알려드립니다.</p>
+										</dd>
+									</dl>
+								</dd>
+							</dl>
+							<dl>
+								<dt>웹사이트 알림 설정</dt>
+								<dd>
+									<dl>
+										<dt>알림 갯수 표시</dt>
+										<dd>읽지 않은 웹사이트 알림 갯수를 표시합니다.</dd>
+										<dd><img src="../img/website_alarm.png" alt="#"></dd>
+									</dl>
+								</dd>
+								<dd>
+									<dl>
+										<dt>알림 갯수 표시</dt>
+										<dd>새로운 알림이 있는 경우 팝업으로 내용을 표시합니다.</dd>
+									</dl>
+								</dd>
+							</dl>
 						</td>						
 					</tr>					
 				</table>

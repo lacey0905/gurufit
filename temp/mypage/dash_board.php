91400138556781cee0545ceeb37da38b47939db8
user <user@LAPTOP-5K4FIEFV>
2017-08-08 20:47:26 +0900

    0808

--- temp/mypage/dash_board.php
+++ temp/mypage/dash_board.php
@@ -48,18 +48,219 @@
 							</li>
 							<li>
 								<a href="#">
-									<img src="" alt="">
+									<img src="../img/review-1.png" alt="">
 									<span>사진</span>
 								</a>
 							</li>
 							<li>
 								<a href="#">
-									<img src="" alt="">
+									<img src="../img/review-1.png" alt="">
 									<span>소셜</span>
 								</a>
 							</li>
 						</ul>
 					</div>
+				</div>
+				<div class="dash_sec dash_x3">
+					<h3 class="dash_tit">경험치 현황</h3>
+					<div class="deps_gauge_wrap">
+						<strong class="deps_gauge_msg">
+							다음 등급 <em>Silver III</em> 까지 <em>1000xp</em> 남음
+						</strong>
+    					<div class="deps_gauge">
+    						<div class="current" style="width:50%;">
+    							<div class="msg_box_a level_info">
+        							<span class="msg">1000xp</span>
+        							<div class="shadow"></div>
+        						</div>
+    						</div>		
+    						<div class="target" style="width:75%;"></div>
+    					</div>
+    					<strong class="deps_gauge_msg">
+							현재 등급 유지에 필요한 경험치 <em>800xp</em> 남음
+						</strong>
+					</div>
+					<dl class="level_up_guide">
+						<dt><a href="#">경험치를 얻을 수 있는 방법</a></dt>
+						<dd><a href="#">구매 상품평 등록</a></dd>
+						<dd><a href="#">구매 상품 View 등록</a></dd>
+						<dd><a href="#">구매 내역 공유</a></dd>
+						<dd><a href="#">추천인 회원 가입</a></dd>
+						<dd><a href="#">이메일 수신 동의</a></dd>
+						<dd class="more"><a href="#">더보기</a></dd>
+					</dl>
+					
+					<div class="coupon_count_box">
+						<strong class="box_tit">쿠폰<a href="#" class="more">보유 쿠폰 보기</a></strong>
+						<div class="count_box">
+							<div class="box_sec count"><em>10</em>장</div>
+							<div class="box_sec price"><em>28,000</em>원</div>
+						</div>
+					</div>
+					
+				</div>
+				<div class="dash_sec dash_x3">
+					<h3 class="dash_tit">
+						FUN뱃지
+						<a href="#" class="more">FUN뱃지 미션보기</a>
+					</h3>
+					<div class="fun_badge_list">
+						<ul>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>하트 x 20</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>뉴비</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>굿픽쳐20</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>하트 x 20</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>뉴비</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>굿픽쳐20</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>하트 x 20</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>뉴비</span>
+							</li>
+							<li>
+								<img src="../img/fun_badge_01.png" alt="">
+								<span>굿픽쳐20</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+			<div class="dash_tab_wrap">
+				<ul class="tab_nav">
+					<li class="active"><a href="#">나의 리뷰</a></li>
+					<li><a href="#">나의 사진</a></li>
+					<li><a href="#">소셜 활동</a></li>
+				</ul>
+				<div class="tab_cont">
+					<ul>
+						<li>
+							<strong class="cate">사진</strong>
+							<em class="count">172</em>
+							<span class="rank_tit">사진 중 상위</span>
+							<span class="rank_count">12%</span>
+						</li>
+						<li>
+							<strong class="cate">베스트 리뷰</strong>
+							<em class="count">36</em>
+							<span class="rank_tit">사진 중 상위</span>
+							<span class="rank_count">63%</span>
+						</li>
+						<li>
+							<strong class="cate">공감</strong>
+							<em class="count">84</em>
+							<span class="rank_tit">사진 중 상위</span>
+							<span class="rank_count">52%</span>
+						</li>
+					</ul>
+				</div>
+			</div>
+			
+			
+			<div class="review_box_list">
+				<div class="review_box_head">
+					<div class="btn_right">
+						<a href="#" class="">새로운 리뷰 등록</a>
+					</div>
+				</div>
+				<ul>
+					<li>
+						<div class="review_list_thum">
+							<img src="../img/review_thum_x100.png" alt="" class="thum">
+							<span class="tit">구입한 날짜</span>
+							<em class="cont">2017년 7월 10일</em>
+						</div>
+						<div class="review_list_box">
+							<div class="review_cont">
+								<div class="rating">
+		    						<strong class="star_rating">
+		    							<i class="star_on"></i>
+		    							<i class="star_on"></i>
+		    							<i class="star_on"></i>
+		    							<i class="star_on"></i>
+		    							<i class="star_off"></i>
+		   							</strong>
+		    					</div>
+								<span class="date">1주일 전 구매</span>
+								<p class="cont">
+									구루핏 첫 구매인데 좋네요. 추천한 275사이즈 구매자입니다.
+									다른 신발은 270사이즈를 매번 신었는데 슈포래빗의 사이즈로 구매하니 제 발에 딱 맞고 핏감이 좋네요. 다른 신발은 270사이즈를 매번 신었는데 슈포래빗의 사이즈로 구매하니 제 발에 딱 맞고 핏감이 좋네요. 다른 신발은 270사이즈를 매번 신었는데 슈포래빗의 사이즈로 구매하니 제 발에 딱 맞고…
+								</p>
+								<a href="#" class="cont_more">더 보기</a>
+							</div>
+							<ul class="like">
+								<li><a href="#" class="like_btn up">52</a></li>
+								<li><a href="#" class="like_btn dw">4</a></li>
+								<li><a href="#" class="report">신고하기</a></li>
+							</ul>
+						</div>
+						<div class="review_list_gauge">
+							<div class="step_gauge">
+								<ul class="gauge_area">
+									<li>
+										<em>길이</em>
+										<div class="gauge">
+											<div class="step_bar">
+												<span class="bar step_04"></span>
+												<span class="step_dot fir"></span>
+												<span class="step_dot sec"></span>
+											</div>
+											<span class="gauge_min">작은편</span>
+											<span class="gauge_max">큰편</span>
+										</div>
+									</li>
+									<li>
+										<em>발 폭</em>
+										<div class="gauge">
+											<div class="step_bar">
+												<span class="bar step_05"></span>
+												<span class="step_dot fir"></span>
+												<span class="step_dot sec"></span>
+											</div>
+											<span class="gauge_min">좁은 편이에요</span>
+											<span class="gauge_max">넉넉해요</span>
+										</div>
+									</li>
+									<li>
+										<em>편안함</em>
+										<div class="gauge">
+											<div class="step_bar">
+												<span class="bar step_01"></span>
+												<span class="step_dot fir"></span>
+												<span class="step_dot sec"></span>
+											</div>
+											<span class="gauge_min">오래 신으면 불편해요</span>
+											<span class="gauge_max">하루 종일 신어도 편해요</span>
+										</div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<div class="review_list_foot">
+					<a href="#" class="review_more">공감 리뷰 더 보기</a>
 				</div>
 			</div>
 			

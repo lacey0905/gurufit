09d4789c2207d398e76ab3c927711f5ff0e9ea5a
yeseo <yeseo@shoe4rabbit.com>
2017-08-10 14:28:32 +0900

    상세페이지 디테일

---
+++ temp/product/contents_detail.php
@@ -0,0 +1,54 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<section class="contents_inner">
+		<article class="contents_top_tit">
+			<div class="contents_top_tit_inner">
+				<em>
+					<span>구루Zone</span> ㅣ 구루Think
+					<i class="material-icons">&#xE87E;</i>
+				</em>
+				<h2>궁금하면 눌러봐 No.1 (시리즈)</h2>
+				<h3>나만 몰랐던 이야기 feat.아이유</h3>
+				<div class="name_social_line">
+					<p>By<span>ENFP</span> ·  2017.10.17</p>
+					<dl class="share">
+						<dt><em>41</em> Shares</dt>
+						<dd><a href="#" class="icon_kakao_x20">KAKAO</a></dd>
+						<dd><a href="#" class="icon_facebook_x18">FACEBOOK</a></dd>
+						<dd><a href="#" class="icon_insta_x18">INSTAGRAM</a></dd>
+					</dl>	
+				</div>
+			</div>
+		</article>
+		<article class="contents_middle">
+			<div class="contents_big_photo_area">
+				<img src="../img/con_d1.png" alt="상세썸네일">				
+			</div>
+			<div class="contents_text_one">
+				<p>
+				집을 가는 지하철 안 이었는데 
+				그날 따라 <span>유난히 사람들의 신발</span>이 
+				눈에 들어왔었다. 					
+				</p>
+				<p>
+				그러다가 어떤 한 남자가 엄청 큰 배낭을 들고 있었는데 살짝 열린 가방 문 사이로 신발이 눈에 띄었다. 그러다가 내가 여행 했을 때를 생각해봤는데 여행가방 쌀 때 신발이 큰 문제 였던 것 같다.
+				여행 가방 쌀 때 운동화 하나, 샌들 하나, 아니면 혹시 모르는 일을 대비 해 좋은 구두 하나 등 행선지에 따라 신발을 넣다보면 어느 순간 가방이 꽉 차버린다. 세 켤레면 아무것도 아닌 것 같지만 아무것도 아닌 게 아니라 문제다. 그래서 항상 눈물을 머금고 “내가 뭐 구두신고 좋은 곳을 가겠어?” 라고 중얼거리면서 구두를 빼고 짐을 싸면…항상 좋은 곳을 가게 되더라…하하..나만 그런가…삶이 머피의 법칙이다..
+				그런데!!!!!!내가 지금 소개해 줄 신발은 완전 신기한 신발이다. 나만 몰랐던 이야기 feat.아이유 일 수도 있지만. 이제 드디어 신발 신기한 신발 시리즈를 시작해 보겠다. 많이 기다리게 해서 미안하다.<br>
+				<span>(두구두구두구두구)</span>					
+				</p>
+			</div>
+		</article>	
+	</section>
+
+</section>
+<?php include "../inc/footer.php" ?>
+ 

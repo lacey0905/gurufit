d96378b06bf495dc0d48baf4adb6c5811f2762ac
user <user@LAPTOP-5K4FIEFV>
2017-08-10 17:15:00 +0900

    combine

---
+++ temp/product/combine_list.php
@@ -0,0 +1,233 @@
+<?php include "../inc/header.php" ?>
+<section class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">스토어</a></dd>
+				<dd><a href="#">남자</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<article class="sub_content custom_inner">
+		<section class="sub_head">
+			<h2 class="s_lar">Search</h2>
+			<!-- 검색 키워드 -->
+			<div class="search_wrap">
+    			<dl class="search_result result_tag">
+    				<dt>총 <strong class="search_count">3,517</strong>개의 검색 결과가 있습니다.</dt>
+    				<dd class="tag_bg_a"><a href="#">KimHayoung</a></dd>
+    				<dd class="tag_bg_b"><a href="#">주황</a></dd>
+    				<dd class="tag_bg_b"><a href="#">Nike</a></dd>
+    				<dd class="tag_bg_b"><a href="#">스포츠</a></dd>
+    			</dl>
+    			<a href="#" class="search_opt_btn btn_arr_a btn_hover_a">검색 저장</a>
+			</div>
+		</section>
+		<section class="content_area">
+			<!-- 진열 영역 -->
+			<article class="custom_prd prd_type_b">
+				<h3 class="search_count"><em>스토어</em>에서 <em class="count">117</em>개의 결과를 찾았습니다.</h3>
+				<ul class="x5 opt_pop">
+					<!-- [active] = 옵션 정보가 켜져 있는 상태 -->
+					<li class="active">
+						<figure>
+	    					<span class="thum">
+	    						<!-- 나의 리스트에 담기면 [check] 클래스 추가 -->
+	    						<a href="#" class="my_list_btn check">나의 리스트에 담기</a>
+	    						<a href="#">
+	    							<img src="../img/store_sample_230x160.png" alt="샘플상품">
+    							</a>
+   							</span>
+   							<!-- 별점 리스트  [star_on] 노란색 별, [star_off] 회색별 -->
+	    					<div class="rating">
+	    						<strong class="star_rating">
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_off"></i>
+	   							</strong>
+	    						<span class="cnt">(29)</span>
+	    						<span class="sale">Sale 40%</span>
+	    					</div>
+	    					<figcaption>
+	    						<span class="subject">NIKE FREE 5.0</span>
+	    						<span class="price">
+	    							<strong class="sell_price">89,100<em>원</em></strong>
+	    							<del class="customer_price">99,000원</del>
+	    						</span>
+	    					</figcaption>
+	    					<div class="open_detail">
+    							<div class="detial_color_list">
+    								<!-- 컬러 리스트  -->
+    								<ul>
+    									<li class="c_black">Black</li>
+    									<li class="c_blue">Blue</li>
+    									<li class="c_red">Red</li>
+    									<li class="c_yellow">Yellow</li>
+    									<li class="c_white">White</li>
+    								</ul>
+    							</div>
+    							<div class="detial_opt_list">
+    								<!-- 
+    								    사이즈 리스트 
+    								  [active] = 선택 된 사이즈
+    								  [dis] = 재고가 없는 사이즈        
+    								-->
+									<ul class="check_opt_box">
+										<li class=""><a href="#">230</a></li>
+										<li><a href="#">235</a></li>
+										<li class="dis"><a href="#">240</a></li>
+										<li class="dis"><a href="#">245</a></li>
+										<li><a href="#">250</a></li>
+										<li><a href="#">255</a></li>
+										<li><a href="#">260</a></li>
+										<li class=""><a href="#">265</a></li>
+										<li><a href="#">270</a></li>
+										<li><a href="#">275</a></li>
+										<li><a href="#">280</a></li>
+										<li><a href="#">285</a></li>
+										<li class=""><a href="#">290</a></li>
+										<li><a href="#">295</a></li>
+										<li><a href="#">300</a></li>
+										<li><a href="#">305</a></li>
+									</ul>
+    							</div>
+    						</div>
+						</figure>
+					</li>
+					<li class="">
+						<figure>
+	    					<span class="thum">
+	    						<a href="#" class="my_list_btn">나의 리스트에 담기</a>
+	    						<a href="http://lacey.dothome.co.kr/yeseo/gurufit/product/store_detail.php">
+	    							<img src="../img/store_sample_230x160.png" alt="샘플상품">
+    							</a>
+   							</span>
+	    					<div class="rating">
+	    						<strong class="star_rating">
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_on"></i>
+	    							<i class="star_off"></i>
+	   							</strong>
+	    						<span class="cnt">(29)</span>
+	    						<span class="sale">Sale 40%</span>
+	    					</div>
+	    					<figcaption>
+	    						<span class="subject">NIKE FREE 5.0</span>
+	    						<span class="price">
+	    							<strong class="sell_price">89,100<em>원</em></strong>
+	    							<del class="customer_price">99,000원</del>
+	    						</span>
+	    					</figcaption>
+	    					<div class="open_detail">
+    							<div class="detial_color_list">
+    								<ul>
+    									<li class="c_black">Black</li>
+    									<li class="c_blue">Blue</li>
+    									<li class="c_red">Red</li>
+    									<li class="c_yellow">Yellow</li>
+    									<li class="c_white">White</li>
+    								</ul>
+    							</div>
+    							<div class="detial_opt_list">
+									<ul class="check_opt_box">
+										<li class=""><a href="#">230</a></li>
+										<li><a href="#">235</a></li>
+										<li class="dis"><a href="#">240</a></li>
+										<li><a href="#">245</a></li>
+										<li><a href="#">250</a></li>
+										<li><a href="#">255</a></li>
+										<li><a href="#">260</a></li>
+										<li class=""><a href="#">265</a></li>
+										<li><a href="#">270</a></li>
+										<li class="dis"><a href="#">275</a></li>
+										<li><a href="#">280</a></li>
+										<li><a href="#">285</a></li>
+										<li class=""><a href="#">290</a></li>
+										<li><a href="#">295</a></li>
+										<li class="dis"><a href="#">300</a></li>
+										<li><a href="#">305</a></li>
+									</ul>
+    							</div>
+    						</div>
+						</figure>
+					</li>
+				</ul>
+				<div class="prd_more">
+    				<a href="#" class="btn_hover_a"><em>스토어</em>에서 더보기</a>
+    			</div>
+			</article>
+			<!-- 진열 영역 -->
+			<article class="custom_prd prd_type_a">
+				<h3 class="search_count"><em>매거진</em>에서 <em class="count">117</em>개의 결과를 찾았습니다.</h3>
+				<ul class="x4">
+        			<li>
+        				<a href="#">
+        					<figure>
+        						<img src="../img/prd_sample.png" alt="샘플상품">
+        						<figcaption>
+        							<span class="kind">인터뷰</span>
+        							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
+        							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
+        							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
+        							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
+        						</figcaption>
+        					</figure>
+        				</a>
+        			</li>
+        			<li>
+        				<a href="#">
+        					<figure>
+        						<img src="../img/prd_sample.png" alt="샘플상품">
+        						<figcaption>
+        							<span class="kind">인터뷰</span>
+        							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
+        							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
+        							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
+        							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
+        						</figcaption>
+        					</figure>
+        				</a>
+        			</li>
+        			<li>
+        				<a href="#">
+        					<figure>
+        						<img src="../img/prd_sample.png" alt="샘플상품">
+        						<figcaption>
+        							<span class="kind">인터뷰</span>
+        							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
+        							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
+        							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
+        							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
+        						</figcaption>
+        					</figure>
+        				</a>
+        			</li>
+        			<li>
+        				<a href="#">
+        					<figure>
+        						<img src="../img/prd_sample.png" alt="샘플상품">
+        						<figcaption>
+        							<span class="kind">인터뷰</span>
+        							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
+        							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
+        							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
+        							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
+        						</figcaption>
+        					</figure>
+        				</a>
+        			</li>
+        		</ul>
+				<div class="prd_more">
+    				<a href="#" class="btn_hover_a"><em>매거진</em>에서 더보기</a>
+    			</div>
+			</article>
+		</section>
+	</article>
+</section>
+<?php include "../inc/footer.php" ?>

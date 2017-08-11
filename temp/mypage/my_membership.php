80dcfd9d6cfc7c62afb45b99d80733b7bccdfa9e
user <user@LAPTOP-5K4FIEFV>
2017-08-11 17:32:03 +0900

    hyeon_0811

---
+++ temp/mypage/my_membership.php
@@ -0,0 +1,192 @@
+<?php include "../inc/header.php" ?>
+<section class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">스토어</a></dd>
+				<dd><a href="#">남자</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<article class="sub_content custom_inner">
+		<section class="sub_head">
+			<h2>My Page</h2>
+			<div class="tab_active_type ">
+    			<ul>
+    				<li><a href="#">대시보드</a></li>
+    				<li><a href="#">멤버십</a></li>
+    				<li><a href="#">알림</a></li>
+    				<li><a href="#">새로운 활동</a></li>
+    				<li class="active"><a href="#">나의 리스트</a></li>
+    				<li><a href="#">히스토리</a></li>
+    				<li><a href="#">1:1문의</a></li>
+    				<li><a href="#">개인정보 설정</a></li>
+    				<li><a href="#">구매내역</a></li>										
+    			</ul>
+    		</div>
+		</section>
+		<h3 class="tit_sub_a">나의 멤버쉽</h3>
+		<section class="membership_dash">
+			<h4 class="tit_sub_c">나의 멤버쉽 등급</h4>
+			<div class="membership_cont">
+    			<div class="ms_level">
+    				<strong class="badge_level bronze">
+    					<span>BRONZE<br>I</span>
+    				</strong>
+    				<span class="user_name">
+    					<em>KimHayoung</em>
+    					님의 멤버쉽 I
+    				</span>
+    				<span class="date">
+    					발행일 : 2017.07.12  /  유효기간 : 2018. 08. 12
+    				</span>
+    			</div>
+    			<div class="ms_gauge">
+    				<strong class="membership_tit">경험치 현황</strong>
+    				<div class="deps_gauge_wrap">
+    					<strong class="deps_gauge_msg">
+    						다음 등급 <em>Silver III</em> 까지 <em>1000xp</em> 남음
+    					</strong>
+    					<div class="deps_gauge">
+    						<div class="current" style="width:50%;">
+    							<div class="msg_box_a level_info">
+        							<span class="msg">1000xp</span>
+        							<div class="shadow"></div>
+        						</div>
+    						</div>		
+    						<div class="target" style="width:75%;"></div>
+    					</div>
+    					<strong class="deps_gauge_msg">
+    						현재 등급 유지에 필요한 경험치 <em>800xp</em> 남음
+    					</strong>
+    				</div>
+    				<dl class="level_up_guide">
+    					<dt><a href="#">경험치를 얻을 수 있는 방법</a></dt>
+    					<dd><a href="#">구매 상품평 등록</a></dd>
+    					<dd><a href="#">구매 상품 View 등록</a></dd>
+    					<dd><a href="#">구매 내역 공유</a></dd>
+    					<dd><a href="#">추천인 회원 가입</a></dd>
+    					<dd><a href="#">이메일 수신 동의</a></dd>
+    					<dd class="more"><a href="#">더보기</a></dd>
+    				</dl>
+    			</div>
+    			<div class="ms_icon">
+    				<strong class="membership_tit">멤버쉽 혜택</strong>
+    				<div class="fun_badge_list">
+    					<ul class="type_b">
+    						<li>
+    							<img src="../img/membership_icon_01.png" alt="">
+    							<span>
+    								<em>90일 이내</em>
+    								무료 교환/반품
+    							</span>
+    						</li>
+    						<li>
+    							<img src="../img/membership_icon_01.png" alt="">
+    							<span>
+    								<em>90일 이내</em>
+    								무료 교환/반품
+    							</span>
+    						</li>
+    						<li>
+    							<img src="../img/membership_icon_01.png" alt="">
+    							<span>
+    								<em>90일 이내</em>
+    								무료 교환/반품
+    							</span>
+    						</li>
+    						<li>
+    							<img src="../img/membership_icon_01.png" alt="">
+    							<span>
+    								<em>90일 이내</em>
+    								무료 교환/반품
+    							</span>
+    						</li>
+    						<li>
+    							<img src="../img/membership_icon_01.png" alt="">
+    							<span>
+    								<em>90일 이내</em>
+    								무료 교환/반품
+    							</span>
+    						</li>
+    					</ul>
+    				</div>
+    			</div>
+			</div>
+			<div class="membership_foot">
+				<strong class="next_level">
+    				<a href="#">다음 등급 혜택<em>Silver III</em></a>
+				</strong>
+				<ul class="membership_link">
+					<li><a href="#">&#183;&nbsp;90일 이내 무료 교환/반품</a></li>
+					<li><a href="#">&#183;&nbsp;당일 출고</a></li>
+					<li><a href="#">&#183;&nbsp;상시할인 1.5%</a></li>
+					<li><a href="#">&#183;&nbsp;Early Access 3순위</a></li>
+					<li><a href="#">&#183;&nbsp;업그레이드 기념 쿠폰</a></li>
+				</ul>
+			</div>
+		</section>
+		<h3 class="tit_sub_a">나의 뱃지</h3>
+		<section class="my_badge_box">
+			<h4 class="tit_sub_c">나의 멤버쉽 등급</h4>
+			<div class="my_badge_inner">
+    			<ul class="my_badge_list">
+    				<li>
+    					<img src="../img/review-1.png" alt="" class="my_badge_icon">
+    					<strong class="my_badge_name">리뷰</strong>
+    					<span class="my_badge_msg">
+    						처음으로 '가장 도움 되는 상품평'에<br>선정 되었습니다.
+    					</span>
+    				</li>
+    				<li class="active">
+    					<img src="../img/review-1.png" alt="" class="my_badge_icon">
+    					<strong class="my_badge_name">리뷰</strong>
+    					<span class="my_badge_msg">
+    						처음으로 '가장 도움 되는 상품평'에<br>선정 되었습니다.
+    					</span>
+    					<em class="my_badge_select">대표 뱃지로 설정중</em>
+    					<div class="my_badge_tip">
+    						<p>다른 회원이 이 뱃지를 보게 됩니다.</p>
+    					</div>
+    				</li>
+    				<li>
+    					<img src="../img/review-1.png" alt="" class="my_badge_icon">
+    					<strong class="my_badge_name">리뷰</strong>
+    					<span class="my_badge_msg">
+    						처음으로 '가장 도움 되는 상품평'에<br>선정 되었습니다.
+    					</span>
+    				</li>
+    			</ul>
+    			<dl class="my_badge_next">
+    				<dt><strong>다음 도전</strong></dt>
+    				<dd><a href="#">더블 엄지척!</a></dd>
+    				<dd><a href="#">샷 파이브</a></dd>
+    				<dd><a href="#">핫 링크</a></dd>
+    			</dl>
+			</div>
+		</section>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	</article>
+</section>
+<?php include "../inc/footer.php" ?>

741c2df54b254b07ead494f239fcc32b0abade2b
yeseo <yeseo@shoe4rabbit.com>
2017-08-11 14:33:51 +0900

    컨텐츠 상세페이지

---
+++ temp/product/contents_detail.php
@@ -0,0 +1,182 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<section class="contents_inner">
+		<article class="contents_top_tit">
+			<div class="contents_top_tit_inner">
+				<em>
+					<span>구루Zone</span> ㅣ 구루Think
+					<i class="material-icons">&#xE87E;</i>
+				</em>
+				<h2>궁금하면 눌러봐 No.1 (시리즈)</h2>
+				<h3>나만 몰랐던 이야기 feat.아이유</h3>
+				<div class="name_social_line">
+					<p>By<span>ENFP</span> ·  2017.10.17</p>
+					<dl class="share">
+						<dt><em>41</em> Shares</dt>
+						<dd><a href="#" class="icon_kakao_x25">KAKAO</a></dd>
+						<dd><a href="#" class="icon_facebook_x23">FACEBOOK</a></dd>
+						<dd><a href="#" class="icon_insta_x22">INSTAGRAM</a></dd>
+					</dl>	
+				</div>
+			</div>
+		</article>
+		<article class="contents_middle">
+			<div class="contents_big_photo_area">
+				<img src="../img/con_d1.png" alt="상세썸네일">				
+			</div>
+			<div class="contents_text_one">
+				<p>
+				집을 가는 지하철 안 이었는데 
+				그날 따라 <span>유난히 사람들의 신발</span>이 
+				눈에 들어왔었다. 					
+				</p>
+				<p>
+				그러다가 어떤 한 남자가 엄청 큰 배낭을 들고 있었는데 살짝 열린 가방 문 사이로 신발이 눈에 띄었다. 그러다가 내가 여행 했을 때를 생각해봤는데 여행가방 쌀 때 신발이 큰 문제 였던 것 같다.
+				여행 가방 쌀 때 운동화 하나, 샌들 하나, 아니면 혹시 모르는 일을 대비 해 좋은 구두 하나 등 행선지에 따라 신발을 넣다보면 어느 순간 가방이 꽉 차버린다. 세 켤레면 아무것도 아닌 것 같지만 아무것도 아닌 게 아니라 문제다. 그래서 항상 눈물을 머금고 “내가 뭐 구두신고 좋은 곳을 가겠어?” 라고 중얼거리면서 구두를 빼고 짐을 싸면…항상 좋은 곳을 가게 되더라…하하..나만 그런가…삶이 머피의 법칙이다..
+				그런데!!!!!!내가 지금 소개해 줄 신발은 완전 신기한 신발이다. 나만 몰랐던 이야기 feat.아이유 일 수도 있지만. 이제 드디어 신발 신기한 신발 시리즈를 시작해 보겠다. 많이 기다리게 해서 미안하다.<br>
+				<span>(두구두구두구두구)</span>					
+				</p>
+			</div>
+			<div class="contents_text_one_line"></div>
+			<div class="contents_video_area">
+				<div class="contents_video">
+                    <div style="position:relative;height:0;padding-bottom:56.25%">
+                        <iframe src="https://www.youtube.com/embed/PW74Tj9xJIQ?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen>
+                        </iframe>
+                    </div>
+				</div>
+				<p>(참고 동영상 삽입한다.. 나도 영어 잘 모르지만 그냥 보면 뭔 말인지 눈치 껏 알 수 있을거다. 알아듯는 척 끄덕이면서 보는 것도 깨알 팁)</p>
+			</div>
+			<div class="contents_text_two">
+				<p>
+                            상황에 따라 다양하게 조립할 수 있는 DIY 모듈 신발 <span>슈즈 Shooz</span>(나 신발이 영어로 뭔 지 아는 사람이다…브랜드이름이 슈즈다..오해는 금지한다..)!!! 
+                            여행자를위한 맞춤 신발이라고 해도 과언이 아니다. 
+                            영국의 디자이너 Edoardo Iannuzzi가 개발한 슈즈는 신발커버와 밑창이 따로 구성되어 있어 용도에 따라 다양한 신발을 조립할 수 있으며 신발 커버의 경우 구두와 운동화, 편상화, 단화 등 4가지 종류가 있다. 디자인은 훨씬 다양하다!
+                            밑창의 경우엔 스니커즈, 운동용, 트래킹 용 등 3가지가 있다.
+            	…가물가물하지만..경우의 수를 생각해서 계산해보면..? 신발 커버와 밑창을 매칭해서 나올 수 있는 조합은 12가지다!! 후후후				
+				</p>
+				<p>
+					<img src="../img/con_d2.png" alt="상세썸네일">	
+				</p>
+			</div>
+			<div class="contents_shoe_list_photo">
+					<img src="../img/con_d3.png" alt="상세썸네일">				
+			</div>
+			<div class="contents_text_three">
+				<p>
+				내가 소개하면서 나만 신기 할 수도 있지만..내 맘대로 골라 신을 수도 있고, 나처럼 여행을 좋아하고 신발 때문에 심장 답답함을 경험 해 본 사람이라면 이 신발 꽤 괜찮은 녀석 같다. 환경을 생각했을 때도 신발을 만드는데 공정이 줄어드니 자연스럽게 에너지도 절약 할 수 있고 매일 스킨을 교체하면서 신으면 신발이 덜 질리니까 더 오래 신을 수도 있지 않을 까 생각한다. 그리고 신발의 스킨은 재활용까지 된다니..뭔가 신발게의 요! 물! 이라는 생각이 든다.<br>
+				<br>
+				오늘은 이것으로 포스팅을 마치도록 하겠다. 다음에 보자.<br>
+				<br>
+				*참고로 ‘Shooz’ 와 나는 1%연관성도 없는 사람이다. 그들은 내가 지구에 태어난 줄도 모를거다 아마. 이거 홍보 글 아니다. 진짜 신기해서다. 오해 말자.<br> 				
+				</p>				
+			</div>
+			<div class="contents_tag_area">
+				<ul>
+					<li><a href="#"><img src="../img/cd_hash1.png" alt="상세썸네일"></a></li>
+					<li><a href="#"><img src="../img/cd_hash2.png" alt="상세썸네일"></a></li>
+					<li><a href="#"><img src="../img/cd_hash3.png" alt="상세썸네일"></a></li>
+					<li><a href="#"><img src="../img/cd_hash4.png" alt="상세썸네일"></a></li>
+				</ul>
+				<dl class="share">
+					<dt><em>41</em> Shares</dt>
+					<dd><a href="#" class="icon_kakao_x25">KAKAO</a></dd>
+					<dd><a href="#" class="icon_facebook_x23">FACEBOOK</a></dd>
+					<dd><a href="#" class="icon_insta_x22">INSTAGRAM</a></dd>
+				</dl>				
+			</div>
+		</article>
+		<article class="contents_bottom">
+			<div class="contents_guru_info_area">
+				<img src="../img/review-3.png" alt="상세썸네일">
+				<p>
+					<strong>불나방스타쏘세지클럽</strong><br>
+					<em>컨텐츠 <span>69</span>개   팔로워 33명</em>
+				</p>
+				<a href="#" class="btn_ btn_x_s2">팔로우 하기</a>
+			</div>
+		</article>	
+		
+		<div id="relatedProduct">
+			<nav class="detail_nav cd_detail_nav">
+				<div class="custom_inner cd_custom_inner">
+					<h4>연관 상품</h4>
+					<span><a href="#">연관 상품 더보기<img src="../img/arrow_half.png" alt="화살표 반"></a></span>
+				</div>
+			</nav>
+			<div class="custom_inner">
+				<article class="prd_type_b">
+    				<ul class="x4">
+    					<li class="">
+    						<figure>
+    	    					<span class="thum">
+    	    						<a href="#" class="my_list_btn"><i class="fa fa-heart-o" aria-hidden="true"></i>나의 리스트에 담기</a>
+    	    						<a href="http://lacey.dothome.co.kr/yeseo/gurufit/product/store_detail.php">
+    	    							<img src="../img/store_sample_230x160.png" alt="샘플상품">
+        							</a>
+       							</span>
+    	    					<div class="rating">
+    	    						<strong class="star_rating">
+    	    							<i class="star_on"></i>
+    	    							<i class="star_on"></i>
+    	    							<i class="star_on"></i>
+    	    							<i class="star_on"></i>
+    	    							<i class="star_off"></i>
+    	   							</strong>
+    	    						<span class="cnt">(29)</span>
+    	    						<span class="sale">Sale 40%</span>
+    	    					</div>
+    	    					<figcaption>
+    	    						<span class="subject">NIKE FREE 5.0</span>
+    	    						<span class="price">
+    	    							<strong class="sell_price">89,100<em>원</em></strong>
+    	    							<del class="customer_price">99,000원</del>
+    	    						</span>
+    	    					</figcaption>
+    						</figure>
+    					</li>
+    				</ul>
+    			</article>
+			</div>
+		</div>
+		<div id="relatedProduct">
+			<nav class="detail_nav cd_detail_nav">
+				<div class="custom_inner cd_custom_inner">
+					<h4>연관 컨텐츠</h4>
+					<span><a href="#">연관 컨텐츠 더보기<img src="../img/arrow_half.png" alt="화살표 반"></a></span>
+				</div>
+			</nav>
+			<div class="custom_inner">
+				<article class="custom_inner custom_prd prd_type_a">
+            		<ul class="x4">
+            			<li>
+            				<a href="#">
+            					<figure>
+            						<img src="../img/prd_sample.png" alt="샘플상품">
+            						<figcaption>
+            							<span class="kind">인터뷰</span>
+            							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
+            							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
+            							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
+            							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
+            						</figcaption>
+            					</figure>
+            				</a>
+            			</li>
+            		</ul>
+            	</article>
+			</div>		
+		</div>
+	</section>
+</section>
+<?php include "../inc/footer.php" ?>
+ 
---
+++ temp/product/contents_detail_another.php
@@ -0,0 +1,183 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<section class="contents_inner">
+		<article class="contents_top_tit">
+			<div class="contents_top_tit_inner">
+				<em>
+					<span>구루Zone</span> ㅣ 구루Think
+					<i class="material-icons">&#xE87E;</i>
+				</em>
+				<h2>미리 엿보는 패션 트렌드No.1 (시리즈)</h2>
+				<h3>Pantsuit Nation</h3>
+				<div class="name_social_line">
+					<p>By<span>Miss J</span> ·  2017.10.17</p>
+					<dl class="share">
+						<dt><em>41</em> Shares</dt>
+						<dd><a href="#" class="icon_kakao_x25">KAKAO</a></dd>
+						<dd><a href="#" class="icon_facebook_x23">FACEBOOK</a></dd>
+						<dd><a href="#" class="icon_insta_x22">INSTAGRAM</a></dd>
+					</dl>	
+				</div>
+			</div>
+		</article>
+		<article class="contents_middle">
+			<div class="contents_big_photo_area vt_contents_big_photo_area">
+				<img src="../img/vt_con_d1.png" alt="상세썸네일">				
+			</div>
+			<div class="contents_text_one vt_contents_text_one">
+				<p>
+				PANT<span></span><br>
+				SUIT<span></span><br>
+				NATION 					
+				</p>
+				<p>
+				지난 미국 대선 시 클린턴 후보가 유세 때마다 팬츠수트를 입은 후 “Pantsuit Nation”이라는 페이지가 
+				개설될 정도로 그 인기가 실로 높았었는데, 여전히 페미니즘을 상징하는 듯한 팬츠수트의 여파는 
+				더 커져 올해 2017 F/W시즌엔 없어 선 안되는 필수 아이템이 되고 만 것 같다. 잠시 지나가는 패션이 
+				아니라 2018년S/S 컬렉션을 보아도 그 인기는 여전 할 것 같은 예감이 든다.
+				2017 F/W 2018 S/S 컬렉션을 보면 발목을 덮는 팬츠수트 사이로 컬러감이 돋보이는 신발이 눈에 띈
+				다. 아직 끝나지 않은 여름 과 다가올 가을 겨울을 위해 컬러감이 돋보이는 신발과 팬츠수트 하나씩 구
+				매 해 놓는다면 패셔니스타는 바로 당신!
+				</p>
+			</div>
+			<div class="contents_text_one_line"></div>
+			<div class="vt_contents_pho1">
+				<img src="../img/vt_con_d2.png" alt="버젼2 상세포토1">
+				<div class="vt_contents_box_one">
+					<div class="vt_con_d_line"></div>
+					<p>
+					Stella McCartney<br>
+					2017 F/W					
+					</p>	
+				</div>			
+			</div>
+			<div class="vt_contents_pho2">
+				<img src="../img/vt_con_d3.png" alt="버젼2 상세포토2">
+				<div class="vt_contents_box_one">
+					<div class="vt_con_d_line"></div>
+					<p>
+					Stella McCartney<br>
+					2017 F/W					
+					</p>	
+				</div>			
+			</div>
+			<div class="vt_contents_pho2 vt_content_last_pho">
+				<img src="../img/vt_con_d4.png" alt="버젼2 상세포토2">
+				<div class="vt_contents_box_one">
+					<div class="vt_con_d_line"></div>
+					<p>
+					Stella McCartney<br>
+					2017 F/W					
+					</p>	
+				</div>			
+			</div>							
+			<div class="contents_tag_area">
+				<ul>
+					<li><a href="#"><img src="../img/cd_hash1.png" alt="상세썸네일"></a></li>
+					<li><a href="#"><img src="../img/cd_hash2.png" alt="상세썸네일"></a></li>
+					<li><a href="#"><img src="../img/cd_hash3.png" alt="상세썸네일"></a></li>
+					<li><a href="#"><img src="../img/cd_hash4.png" alt="상세썸네일"></a></li>
+				</ul>
+				<dl class="share">
+					<dt><em>41</em> Shares</dt>
+					<dd><a href="#" class="icon_kakao_x25">KAKAO</a></dd>
+					<dd><a href="#" class="icon_facebook_x23">FACEBOOK</a></dd>
+					<dd><a href="#" class="icon_insta_x22">INSTAGRAM</a></dd>
+				</dl>				
+			</div>
+		</article>
+		<article class="contents_bottom">
+			<div class="contents_guru_info_area">
+				<img src="../img/review-3.png" alt="상세썸네일">
+				<p>
+					<strong>불나방스타쏘세지클럽</strong><br>
+					<em>컨텐츠 <span>69</span>개   팔로워 33명</em>
+				</p>
+				<a href="#" class="btn_ btn_x_s2">팔로우 하기</a>
+			</div>
+		</article>	
+		
+		<div id="relatedProduct">
+			<nav class="detail_nav cd_detail_nav">
+				<div class="custom_inner cd_custom_inner">
+					<h4>연관 상품</h4>
+					<span><a href="#">연관 상품 더보기<img src="../img/arrow_half.png" alt="화살표 반"></a></span>
+				</div>
+			</nav>
+			<div class="custom_inner">
+				<article class="prd_type_b">
+    				<ul class="x4">
+    					<li class="">
+    						<figure>
+    	    					<span class="thum">
+    	    						<a href="#" class="my_list_btn"><i class="fa fa-heart-o" aria-hidden="true"></i>나의 리스트에 담기</a>
+    	    						<a href="http://lacey.dothome.co.kr/yeseo/gurufit/product/store_detail.php">
+    	    							<img src="../img/store_sample_230x160.png" alt="샘플상품">
+        							</a>
+       							</span>
+    	    					<div class="rating">
+    	    						<strong class="star_rating">
+    	    							<i class="star_on"></i>
+    	    							<i class="star_on"></i>
+    	    							<i class="star_on"></i>
+    	    							<i class="star_on"></i>
+    	    							<i class="star_off"></i>
+    	   							</strong>
+    	    						<span class="cnt">(29)</span>
+    	    						<span class="sale">Sale 40%</span>
+    	    					</div>
+    	    					<figcaption>
+    	    						<span class="subject">NIKE FREE 5.0</span>
+    	    						<span class="price">
+    	    							<strong class="sell_price">89,100<em>원</em></strong>
+    	    							<del class="customer_price">99,000원</del>
+    	    						</span>
+    	    					</figcaption>
+    						</figure>
+    					</li>
+    				</ul>
+    			</article>
+			</div>
+		</div>
+		<div id="relatedProduct">
+			<nav class="detail_nav cd_detail_nav">
+				<div class="custom_inner cd_custom_inner">
+					<h4>연관 컨텐츠</h4>
+					<span><a href="#">연관 상품 더보기<img src="../img/arrow_half.png" alt="화살표 반"></a></span>
+				</div>
+			</nav>
+			<div class="custom_inner">
+				<article class="custom_inner custom_prd prd_type_a">
+            		<ul class="x4">
+            			<li>
+            				<a href="#">
+            					<figure>
+            						<img src="../img/prd_sample.png" alt="샘플상품">
+            						<figcaption>
+            							<span class="kind">인터뷰</span>
+            							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
+            							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
+            							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
+            							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
+            						</figcaption>
+            					</figure>
+            				</a>
+            			</li>
+            		</ul>
+            	</article>
+			</div>		
+		</div>
+	</section>
+
+</section>
+<?php include "../inc/footer.php" ?>
+ 

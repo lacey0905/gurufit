d5fc52361e2dc069be80c40f4aa0ac4b85fcbb7e
user <user@LAPTOP-5K4FIEFV>
2017-08-11 15:27:38 +0900

    상세 페이지 수정

--- temp/product/store_detail.php
+++ temp/product/store_detail.php
@@ -348,18 +348,431 @@
 					</div>
 				</nav>
 				<div class="custom_inner">
-					<!-- 상세페이지 컨텐츠 내용 iframe -->
-    				<iframe src="./detail_frame.php"style='width:120%' scrolling="no" frameborder="0" onLoad="setIFrameHeight(this)"></iframe>
-                    <script>
-                    	// iframe 세로 길이 자동 조절
-                        function setIFrameHeight(obj){
-                            if(obj.contentDocument){
-                                obj.height = obj.contentDocument.body.offsetHeight + 40;
-                            } else {
-                                obj.height = obj.contentWindow.document.body.scrollHeight;
-                            }
-                        }
-                    </script>
+					<!-- 상세 페이지 내용에 삽입 되는 모든 클래스 앞에 [spec_view_] 접두사를 붙입니다. -->
+                    <!-- 제품 상세 페이지 내용 부분  -->
+    				<div class="spec_view_st_product_info_top">
+    					<h3>NIKE Metcon DSX Flyknit</h3>
+    					<p>
+    						93년도의 아이코닉한 러닝화와 엣지 있는 모던한 스타일이 결합하여 신선한 디자인을 선보이는 튜블라 시리즈입니다.<br>
+    						다이아몬드 패턴 디테일의 3D 투톤 멜랑주 니트 갑피가 눈을 사로잡는 룩을 연출합니다.<br>
+    						대비색상 스티치가 돋보이는 부리토 설포가 편안하게 발을 감싸면서 꼭 맞는 핏을 제공합니다. EVA 미드솔이 하루 종일 지속되는 가벼운 쿠셔닝을 전달합니다.
+    					</p>
+    					<ul>
+    						<li>엣지 있는 투톤 니트와 함께 재탄생한 튜블라 시리즈</li>
+    						<li>투톤 플랫 니트 갑피</li>
+    						<li>인체공학적 디자인으로 꼭 맞는 핏을 선사하는 부리토 설포; 반사소재 스티치 디테일</li>
+    						<li>편안한 착용감의 안감 소재</li>
+    						<li>경량의 쿠셔닝을 제공하는 EVA 미드솔</li>
+    						<li>미드솔의 고무 패드</li>
+    						<li>인체공학적 디자인으로 꼭 맞는 핏을 선사하는 부리토 설포; 반사소재 스티치 디테일</li>
+    						<li>편안한 착용감의 안감 소재</li>
+    					</ul>
+    				</div>
+    				<div class="spec_view_product_spec_area">
+    					<h4>상품스팩</h4>						
+    					<ul>
+    						<li>
+    							<span class="spec_view_type">내전</span>
+    							<img src="../img/product_spec.png" alt="#" class="spec_view_test">
+    							<span class="spec_view_explain">과내전-내전</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">쿠셔닝 타입</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">고탄성</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">무게</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">매우 가벼움 283g</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">갑피 소재</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">고탄성</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">안감</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">직물</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">밑창인솔</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">메모리폼</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">미드솔</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">Boost®</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">미드솔</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">EVA</span>
+    						</li>
+    						<li>
+    							<span class="spec_view_type">굽높이</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">보통 3.2cm</span>
+    						</li>
+    						<li class="spec_view_tip1">
+    							<span class="spec_view_type">힐유형</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">스틸레토</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip1">
+    							'킬힐'이라고 하면 바로 스틸레토 Stiletto를 말하는 것입니다. 힐 높이가 5cm이상이고 길고 가느다란 형<br/>
+    							태입니다.						
+    							</div>									
+    						</li>
+    						<li>
+    							<span class="spec_view_type">부츠 목 높이</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">종아리14cm</span>								
+    						</li>
+    						<li>
+    							<span class="spec_view_type">통 둘레</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">매우 좁음 26cm</span>								
+    						</li>
+    						<li class="spec_view_tip2">
+    							<span class="spec_view_type">기능</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">미끄럼방지</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip2">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip3">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">발수</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip3">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip4">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">유연성</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip4">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip5">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">통기성</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip5">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip6">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">향균</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip6">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip7">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">무봉제</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip7">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip8">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">방수</span>
+    							<div class="spec_view_tip_buble spec_view_pec_tip8">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip9">
+    							<span class="spec_view_type">착화환경</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">캐쥬얼</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip9">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip10">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">아웃도어</span>								
+    							<div class="spec_view_tip_buble spec_view_spec_tip10">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>								
+    						</li>
+    						<li class="spec_view_tip11">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">등산</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip11">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip12">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">비즈니스</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip12">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip13">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">라이프스타일</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip13">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip14">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">바다</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip14">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>										
+    						</li>
+    						<li class="spec_view_tip15">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">격식있는 자리</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip15">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>										
+    						</li>
+    						<li class="spec_view_tip16">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">눈</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip16">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>
+    						<li class="spec_view_tip17">
+    							<span class="spec_view_type">&nbsp;</span>
+    							<img src="../img/product_spec.png" alt="#">
+    							<span class="spec_view_explain">비</span>
+    							<div class="spec_view_tip_buble spec_view_spec_tip17">
+    							쉽게 미끄러지지 않는 재질의 아웃솔이 적용되어 있습니다.
+    							</div>									
+    						</li>																																																																																																																																																																																																																								
+    					</ul>
+    					<ul class="spec_view_brand_category">
+    						<li>
+    							<span>브랜드</span>
+    							<span class="spec_view_brand"><img src="../img/product_spec_brand.png" alt="#"></span>
+    						</li>
+    						<li>
+    							<span>모델넘버</span>
+    							<span>730SPW0020-DB4</span>
+    						</li>
+    						<li>
+    							<span>제조사</span>
+    							<span>FILA코리아</span>
+    						</li>
+    						<li>
+    							<span>수입사</span>
+    							<span>(주)화승</span>
+    						</li>
+    						<li>
+    							<span>원산지</span>
+    							<span>대한민국</span>
+    						</li>
+    						<li>
+    							<span>제조국</span>
+    							<span>중국</span>
+    						</li>
+    						<li>
+    							<span>출시시즌</span>
+    							<span>2017 S(여름)</span>
+    						</li>
+    					</ul>																																																																																																																										
+    				</div>
+    				<div class="spec_view_st_product_info_bottom">
+    					<h3>취급 주의 사항</h3>
+    					<p class="spec_view_st_des1">
+    						<span>천연가죽</span>  젖었을 때는 직사일광 또는 불로 말리지 말고 그늘진 곳에서 건조시키세요. 오염
+    						시 부드러운 솔로 닦고, 슈크리너를 사용하세요. 적정한 온도와 습도에서 보관하는 것이 좋습니다.<br>
+    						<span>섬유· 합성가죽</span> 젖었을 때는 그늘진 곳에서 건조시키세요. 기름기가 있는 장소에서 사용을 
+    						피하세요. 불옆에 놓으면 수축 또는 변형이 발생할 수 있습니다. 표면이 오염되면 비눗물을
+    						적신 천을 사용해 닦아내세요. 구두약과 같은 가죽용품을 사용할 필요가 없습니다.
+    					</p>
+    					<p class="spec_view_st_des2">
+    						신발 뒤꿈치를 꺾어 신지 마세요. 제품의 수명 연장을 위해 사용 용도를 주의해 주십시오.
+    						제품 이상으로 인한 피해보상 또는 수선을 원하실 때는 1:1문의 후 구루핏으로 보내주세요.
+    						수선비가 발생할 수 있습니다.<br>
+    						세탁 등 소비자 부주의로 인한 제품 훼손 및 변형, 품질 보증기간(6개월) 경과한 제품의 품질 
+    						이상에 대해서는 보상의 책임을 지지않습니다. 수선이 가능한 경우 실비로 수선하여 드립니
+    						다.
+    					</p>
+    				</div>
+				    <!-- 상세 페이지 내용 CSS -->
+                    <style>
+                    /*상품상세페이지*/
+                    /*상품스펙*/
+                    .spec_view_st_product_info_top > h3 {
+                    	color: #333333;	
+                    	font-size: 24px;	
+                    	line-height: 36px;
+                    	margin:28px 0 23px 0;	
+                    }
+                    .spec_view_st_product_info_top > p {
+                    	color: #646464;	
+                    	font-size: 14px;	
+                    	line-height: 24px;	
+                    	text-align: justify;
+                    	margin-bottom:34px;	
+                    }
+                    .spec_view_st_product_info_top > ul {
+                    	list-style-type:disc;
+                    	padding-left: 20px;	
+                    }
+                    .spec_view_st_product_info_top > ul > li {
+                    	color: #646464;	
+                    	font-family: SpoqaHanSans;	
+                    	font-size: 14px;	
+                    	line-height: 24px;	
+                    	text-align: justify;	
+                    }
+                    .spec_view_product_spec_area {
+                    	margin:60px 0 51px 0;
+                    	position:relative;
+                    }
+                    .spec_view_product_spec_area h4 {
+                    	display:inline-block;
+                    	color: #000000;	
+                    	font-size: 14px;	
+                    	line-height: 24px;
+                    	margin-right:92px;	
+                    }
+                    .spec_view_product_spec_area ul{
+                    	display:inline-block;
+                    	vertical-align:top;
+                    }
+                    .spec_view_product_spec_area ul > li {
+                        position:relative;
+                        list-style-type: none;
+                        cursor:pointer;
+                    }
+                    .spec_view_product_spec_area .spec_view_type {
+                    	display:inline-block;
+                    	width:88px;
+                    	color: #4A4A4A;	
+                    	font-size: 14px;	
+                    	font-weight: 300;	
+                    	line-height: 36px;	
+                    }
+                    .spec_view_product_spec_area .spec_view_explain {
+                    	width: 101px;	
+                    	color: #4A4A4A;	
+                    	font-size: 14px;	
+                    	font-weight: 300;	
+                    	line-height: 36px;	
+                    	text-align: justify;
+                    	margin-left:16px;	
+                    }
+                    .spec_view_product_spec_area .spec_view_brand_category {
+                    	margin-left:138px;
+                    }
+                    .spec_view_product_spec_area .spec_view_brand_category li span {
+                        display:inline-block;	
+                        color: #4A4A4A;
+                        font-size: 14px;	
+                    	font-weight: 300;	
+                    	line-height: 36px;	
+                    	text-align: justify;
+                    }
+                    .spec_view_product_spec_area .spec_view_brand_category li span:nth-child(1) {
+                    	width: 57px;	
+                    }
+                    .spec_view_product_spec_area .spec_view_brand_category li span:nth-child(2){
+                    	width: 140px;	
+                    	color: #4A4A4A;	
+                    	margin-left:23px;	
+                    }
+                    .spec_view_product_spec_area ul .spec_view_brand img {
+                    	width:25px;
+                    	height:15px;
+                    }
+                    .spec_view_product_spec_area ul > li:hover .spec_view_tip_buble {
+                        display:block;
+                    }
+                    .spec_view_tip_buble {
+                    	display:none;
+                    	width: 611px;	
+                    	border: 1px solid #7053FF;	
+                    	border-radius: 4px;	
+                    	background-color: #FFFFFF;	
+                    }
+                    .spec_view_tip_buble:before {
+                    	content:'';
+                    	display:block;
+                    	position:absolute;
+                        top: 32px;
+                        left:-6px;
+                        width: 10px;
+                        height: 10px; 
+                        background: #FFFFFF;
+                        border-right:1px solid #7053FF;
+                        border-bottom:1px solid #7053FF;
+                        transform:rotate(-223deg);
+                        -moz-transform:rotate(-223deg);
+                        -webkit-transform:rotate(-223deg);    	
+                    }
+                    .spec_view_product_spec_area ul > li:hover .spec_view_tip_buble {
+                        position:absolute;
+                    	top:0px;
+                    	left:280px;
+                    	color: #4A4A4A;
+                    	font-size: 14px;	
+                    	font-weight: 300;	
+                    	line-height: 24px;
+                    	padding:5px 0 5px 20px;
+                    }
+                    .spec_view_product_spec_area ul > li:hover .spec_view_tip_buble:before {
+                        top:50%;
+                        margin-top:-7px;
+                    }
+                    .spec_view_st_product_info_bottom:after {
+                    	content:"";
+                    	clear:both;
+                    	display:block;
+                    }
+                    .spec_view_st_product_info_bottom > h3 {
+                    	color: #000000;	
+                    	font-size: 16px;	
+                    	line-height: 21px;
+                    	margin-bottom:7px;	
+                    }
+                    .spec_view_st_product_info_bottom > p {
+                    	width:490px;
+                    	height:144px;
+                    	color: #000000;		
+                    	font-size: 14px;	
+                    	font-weight: 300;	
+                    	line-height: 24px;	
+                    	text-align: justify;
+                    	float:left;	
+                    }
+                    .spec_view_st_product_info_bottom > p > span {
+                    	color: #000000;	
+                    	font-size: 14px;	
+                    	line-height: 24px;	
+                    	text-align: justify;
+                    	font-weight:500;
+                    	margin-left:5px;	
+                    }
+                    .spec_view_st_product_info_bottom .spec_view_st_des1 {
+                    	margin-right:20px;
+                    }
+                    </style>
 				</div>
 			</div>
 			<div id="productReview">

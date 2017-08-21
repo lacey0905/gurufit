3a611872d4e7830affdf87b205f142d38a619109
yeseo <yeseo@shoe4rabbit.com>
2017-08-21 19:56:47 +0900

    환불 계좌 정보 변경

---
+++ temp/modal/refund_account_change.php
@@ -0,0 +1,236 @@
+<?php include "../inc/header.php" ?>
+<section class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">스토어</a></dd>
+				<dd><a href="#">남자</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<div class="f_modal_layer open">
+		<div class="f_modal_wrap">
+			<div class="f_modal_area">
+        		<div class="f_modal_head">
+        			<h2>환불 계좌 정보 변경</h2>
+        			<a href="#" class="btn_close">Close</a>
+        		</div>
+        		<div class="f_modal_cont">
+        			<form action="">
+        			<dl class="form_list_a">
+        				<dt>은행</dt>
+        				<dd class="rac_bank_name">
+							groovy@shoeforrabbit.com
+        				</dd>
+        				<dt>예금주</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="text" value="" placeholder="이메일 주소" class="inp_nm_box inp_full" name="">
+								</div>
+							</div>
+        				</dd>
+        				<dt>계좌번호</dt>
+        				<dd>
+							<div class="form">
+	        					<div class="form_inp_area">
+									<input type="text" value="" placeholder="001-00-0000-0000" class="inp_nm_box inp_full" name="">
+								</div>
+							</div>
+        				</dd>
+        			</dl>
+        			</form>
+        		</div>
+        		<div class="f_modal_foot">
+        			<div class="f_modal_btn btn_x2">
+	        			<a href="#" class="btn_ btn_l_l1 btn_hover_a">취소</a>        			
+	        			<a href="#" class="btn_ btn_l_l1 btn_cp btn_hover_shadow">변경 정보 저장</a>
+	        		</div>		
+        		</div>
+    		</div>
+		</div>
+		<div class="f_modal_blind"></div>
+	</div>
+	<div class="f_modal_layer">
+		<div class="f_modal_wrap">
+			<div class="f_modal_area">
+        		<div class="f_modal_head">
+        			<h2>개인 정보 변경</h2>
+        			<a href="#" class="btn_close">Close</a>
+        		</div>
+        		<div class="f_modal_cont">
+        			<form action="">
+        			<dl class="form_list_a">
+        				<dt>이름</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="text" value="" placeholder="이름" class="inp_nm_box inp_full" name="">
+								</div>
+							</div>
+        				</dd>
+        				<dt>닉네임</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="text" value="" placeholder="닉네임" class="inp_nm_box inp_full" name="">
+								</div>
+							</div>
+        				</dd>
+        				<dt>휴대전화번호</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area inp_add_btn inp_r_btn">
+									<input type="text" value="" placeholder="예)  010-1234-5678" class="inp_nm_box" name="">
+									<a href="#" class="btn_ btn_x_m3 btn_hover_a">인증번호 발송</a>      	
+								</div>
+							</div>
+        					<div class="form">
+	        					<div class="form_inp_area inp_add_btn inp_r_btn">
+									<input type="text" value="" placeholder="인증번호" class="inp_nm_box" name="">
+									<span class="msg">인증번호 입력</span>
+								</div>
+							</div>
+        				</dd>
+        				<dt>생년월일</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="text" value="" placeholder="예)  810714" class="inp_nm_box inp_full" name="">
+								</div>
+							</div>
+        				</dd>
+        				<dt>
+        					성별
+        					<div class="form_inline">
+	        					<div class="form_radio custom_radio">
+	    							<label>
+	    								<input type="radio" name="gender">
+	    								<span class="box"></span>
+	    								<span class="label">여성</span>
+	    							</label>
+	    						</div>
+	    						<div class="form_radio custom_radio">
+	    							<label>
+	    								<input type="radio" name="gender" checked="checked"> 
+	    								<span class="box"></span>
+	    								<span class="label">남성</span>
+	    							</label>
+	    						</div>
+    						</div>
+        				</dt>
+        			</dl>
+        			</form>
+        		</div>
+        		<div class="f_modal_foot">
+        			<div class="f_modal_btn btn_x2">
+	        			<a href="#" class="btn_ btn_l_l1 btn_hover_a">취소</a>        			
+	        			<a href="#" class="btn_ btn_l_l1 btn_cp btn_hover_shadow">개인 정보 변경</a>
+	        		</div>		
+        		</div>
+    		</div>
+		</div>
+		<div class="f_modal_blind"></div>
+	</div>
+	<div class="f_modal_layer">
+		<div class="f_modal_wrap">
+			<div class="f_modal_area">
+        		<div class="f_modal_head">
+        			<h2>비밀번호 변경</h2>
+        			<a href="#" class="btn_close">Close</a>
+        		</div>
+        		<div class="f_modal_cont">
+        			<form action="">
+        			<dl class="form_list_a">
+        				<dt>현재 비밀번호</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="password" value="" placeholder="비밀번호" class="inp_nm_box inp_full" name="join_password">
+								</div>
+							</div>
+        				</dd>
+        				<dt>새로운 비밀번호<span class="msg">영문·숫자·특수문자 포함 6자 ~ 20자</span></dt>
+        				<dd>
+        					<div class="form">
+        						<div class="form_inp_area">
+									<input type="password" value="" placeholder="새로운 비밀번호" class="inp_nm_box inp_full" name="join_password">
+								</div>
+								<div class="form_inp_area">
+									<input type="password" value="" placeholder="새로운 비밀번호 확인" class="inp_nm_box inp_full" name="join_password">
+								</div>
+							</div>
+        				</dd>
+        				<dt>비밀번호 안전도<span class="safe">최고 수준 보안</span></dt>
+        				<dd>
+        					<div class="prog">
+	        					<div class="progress_a">
+									<div class="prog_wrap high">
+										<div class="prog_mask">
+	    									<div class="prog_bar" style="width:80%;"></div>
+	    								</div>
+									</div>
+								</div>
+							</div>
+        				</dd>
+        			</dl>
+        			</form>
+        		</div>
+        		<div class="f_modal_foot">
+        				<div class="f_modal_btn btn_x2">
+	        				<a href="#" class="btn_ btn_l_l1 btn_hover_a">취소</a>        			
+	        				<a href="#" class="btn_ btn_l_l1 btn_cp btn_hover_shadow">비밀번호 저장</a>
+	        			</div>		
+        		</div>
+    		</div>
+		</div>
+		<div class="f_modal_blind"></div>
+	</div>
+	<div class="f_modal_layer">
+		<div class="f_modal_wrap">
+			<div class="f_modal_area">
+        		<div class="f_modal_head">
+        			<h2>이메일 변경</h2>
+        			<a href="#" class="btn_close">Close</a>
+        		</div>
+        		<div class="f_modal_cont">
+        			<form action="">
+        			<dl class="form_list_a">
+        				<dt>기존 이메일</dt>
+        				<dd><span class="txt">groovy@shoeforrabbit.com</span></dd>
+        				<dt>새로운 이메일</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="text" value="" placeholder="이메일 주소" class="inp_nm_box inp_full" name="join_password">
+								</div>
+								<div class="form_inp_area">
+									<input type="text" value="" placeholder="이메일 주소 확인" class="inp_nm_box inp_full" name="join_password">
+								</div>
+							</div>
+        				</dd>
+        				<dt>비밀번호</dt>
+        				<dd>
+        					<div class="form">
+	        					<div class="form_inp_area">
+									<input type="password" value="" placeholder="비밀번호" class="inp_nm_box inp_full" name="join_password">
+								</div>
+							</div>
+        				</dd>
+        			</dl>
+        			</form>
+        		</div>
+        		<div class="f_modal_foot">
+        				<div class="f_modal_btn btn_x2">
+	        				<a href="#" class="btn_ btn_l_l1 btn_hover_a">취소</a>        			
+	        				<a href="#" class="btn_ btn_l_l1 btn_cp btn_hover_shadow">이메일 저장</a>
+	        			</div>		
+        		</div>
+    		</div>
+		</div>
+		<div class="f_modal_blind"></div>
+	</div>	
+</section>
+<?php include "../inc/footer.php" ?>

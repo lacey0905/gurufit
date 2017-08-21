f4fe7a82153b36807ffc421ba6bff8db4c283550
yeseo <yeseo@shoe4rabbit.com>
2017-08-21 15:32:46 +0900

    점심이후 수정

---
+++ temp/modal/shipping_choose.php
@@ -0,0 +1,135 @@
+<?php include "../inc/header.php" ?>
+<section class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">스토어</a></dd>
+				<dd><a href="#">남자</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<div class="f_modal_layer big open">
+		<div class="f_modal_wrap">
+			<div class="f_modal_area">
+        		<div class="f_modal_head">
+        			<h2>취소상세정보</h2>
+        			<a href="#" class="btn_close">Close</a>
+        		</div>
+        		<div class="f_modal_cont">
+					<div class="ship_choose_box sc_grey">
+						<table class="ship_choose_tb  grey">
+    						<caption class="blind">배송지 선택</caption>
+    						<colgroup>
+    							<col width="56px">
+    							<col width="694px">
+    						</colgroup>							
+							<tr>
+								<td class="tb_radio">
+                					<div class="table_radio custom_radio">
+                						<label>
+                							<input type="radio" checked="checked" name="addr">
+                							<span class="box"></span>
+                						</label>  						
+                					</div>								
+								</td>
+								<td class="tb_addition">
+									<strong>김하영</strong><span>ㅣ    010-7777-7777   ㅣ    010-7777-7777</span>
+									<p>478-222</p>
+									<p>
+                                                                    서울시 서초구 방배로 22길 11 노블레스 빌딩 3층<br>
+                                                                    슈포래빗 1402동 1703호									
+									</p>
+            						<div class="custom_cbox cbox_d sc_input_box">
+                    					<label>
+                        					<input type="checkbox" id="" name="" value="">
+                        					<span class="box"></span>
+                        					<span class="label">기본 배송지</span>
+                        				</label>
+                    				</div>																	
+								</td>
+							</tr>
+						</table>
+					</div>
+					<div class="ship_choose_box">
+						<table class="ship_choose_tb">
+    						<caption class="blind">배송지 선택</caption>
+    						<colgroup>
+    							<col width="56px">
+    							<col width="694px">
+    						</colgroup>							
+							<tr>
+								<td class="tb_radio">
+                					<div class="table_radio custom_radio">
+                						<label>
+                							<input type="radio" checked="checked" name="addr">
+                							<span class="box"></span>
+                						</label>  						
+                					</div>								
+								</td>
+								<td class="tb_addition">
+									<strong>이봉걸</strong><span>ㅣ    010-7777-7777   ㅣ    010-7777-7777</span>
+									<p>478-222</p>
+									<p>
+                                                                    서울시 서초구 방배로 22길 11 노블레스 빌딩 3층<br>
+                                                                    슈포래빗 1402동 1703호									
+									</p>
+            						<div class="custom_cbox cbox_d sc_input_box">
+                    					<label>
+                        					<input type="checkbox" id="" name="" value="">
+                        					<span class="box"></span>
+                        					<span class="label">기본 배송지</span>
+                        				</label>
+                    				</div>																	
+								</td>
+							</tr>
+						</table>
+					</div>
+					<div class="ship_choose_box">
+						<table class="ship_choose_tb">
+    						<caption class="blind">배송지 선택</caption>
+    						<colgroup>
+    							<col width="56px">
+    							<col width="694px">
+    						</colgroup>							
+							<tr>
+								<td class="tb_radio">
+                					<div class="table_radio custom_radio">
+                						<label>
+                							<input type="radio" checked="checked" name="addr">
+                							<span class="box"></span>
+                						</label>  						
+                					</div>								
+								</td>
+								<td class="tb_addition">
+									<strong>김미남</strong><span>ㅣ    010-7777-7777   ㅣ    010-7777-7777</span>
+									<p>478-222</p>
+									<p>
+                                                                    서울시 서초구 방배로 22길 11 노블레스 빌딩 3층<br>
+                                                                    슈포래빗 1402동 1703호									
+									</p>
+            						<div class="custom_cbox cbox_d sc_input_box">
+                    					<label>
+                        					<input type="checkbox" id="" name="" value="">
+                        					<span class="box"></span>
+                        					<span class="label">기본 배송지</span>
+                        				</label>
+                    				</div>																	
+								</td>
+							</tr>
+						</table>
+					</div>										
+				</div>
+        		<div class="f_modal_foot sc_modal_foot">
+        			<div class="f_modal_btn btn_x2 ">     			
+        				<a href="#" class="btn_ btn_xl_l2">취소</a>
+        				<a href="#" class="btn_ btn_xl_l2 btn_cp btn_hover_shadow">선택완료</a>
+        			</div>		
+        		</div>
+    		</div>
+		</div>
+		<div class="f_modal_blind"></div>
+	</div>
+</section>
+<?php include "../inc/footer.php" ?>

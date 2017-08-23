fafe5aa6b68039b54479abe8cbe4ce9c95501e2d
yeseo <yeseo@shoe4rabbit.com>
2017-08-23 20:28:18 +0900

    수정

---
+++ temp/inc/footer_detail.php
@@ -0,0 +1,362 @@
+	<link rel="stylesheet" type="text/css" href="../css/import/reset.css">
+	<link rel="stylesheet" type="text/css" href="../css/import/font.css">
+	<link rel="stylesheet" type="text/css" href="../css/with.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../css/yeseo.css">
+
+
+<footer id="footer">
+	<section id="foot_detail">
+		<article>
+			<div class="footer_detail_wrapper">
+				<div class="fd_ship_info">
+					<dl class="fd_grade_badge">
+						<dt><img src="../img/fd_bronze-3.png" alt="#" /></dt>
+						<dd>
+							<em>브론즈III</em>
+							멤버십 혜택
+						</dd>
+					</dl>
+					<div class="fd_ship_info_inner">
+						<h4>배송/교환/환불 안내</h4>
+						<dl>
+							<dt><span class="day">익일</span></dt>
+							<dd class="in_order_info">
+								<p>
+								<em>주문/배송 안내</em>
+								・주문 금액에 상관 없이 무료로 배송해 드립니다<br>
+								・결제한 상품을 <span class="fd_line">익일 출고</span>합니다									
+								</p>
+							</dd>
+							<dt><span class="num">30</span></dt>
+							<dd class="in_change_info">
+								<p>
+									<em>교환 안내</em>
+									・<span class="fd_line">출고 후 30일 이내</span>에 무료로 교환할 수 있습니다<br>
+									・교환 기간 중엔 별도의 교환 배송비가 없습니다 <br>
+									・사이즈 교환, 단순 변심에 의한 교환도 가능합니다
+								</p>
+							</dd>
+							<dt><span class="tester">검수</span></dt>
+							<dd class="in_refund_info">
+								<em>환불 안내</em>
+								<p>
+								・<span class="fd_line">상품 검수 후</span> 환불해 드립니다<br>
+								・반품 시 차감되는 금액 없이 전액 환불해 드립니다<br>
+								・환불 시점<br>
+									<ul>	
+									    <li> — 취소 : 취소 완료 즉시 환불 처리됩니다</li>
+									     <li>— 반품 : 상품 검수 후 환불 처리됩니다</li>
+								     </ul> 
+								・결제 수단 별로 환불 시점이 다릅니다 <br>
+								    <a href="#"> 결제 수단 별 환불 시점 자세히 보기</a> 								
+								</p>
+							</dd>							
+						</dl>
+					</div>
+				</div>
+				<div class="fd_ship_method">
+					<h4>교환/환불 신청 방법</h4>
+					<dl>
+						<dt class="fd_website"><img src="../img/fd_website.png" alt="#" /></dt>
+						<dd class="website">
+							<em>웹사이트</em>
+							・마이페이지 구매내역에서 교환  신청할 수 있습니다<br>
+							<a href="#">구매내역으로 이동</a>							
+						</dd>
+						<dt class="fd_qr"><img src="../img/fd_qr-code.png" alt="#" /></dt>
+						<dd class="qr_code">
+							<em>QR코드 이용</em>
+							・배송 박스에 포함된 QR코드를 이용해 쉽게 교환 신청할 수 있습니다<br>
+							・여러 스마트폰 앱에서 QR코드를 읽을 수 있습니다<br>
+							<p>
+						     —  <a href="#">네이버 앱</a> : 검색창 카메라 아이콘 선택 후 QR-바코드 선택<br>
+						     —  <a href="#">페이스북 앱</a> : 더 보기 아이콘 선택 후 QR코드 선택<br>
+						     </p>							
+						</dd>
+						<dt class="fd_warning"><img src="../img/fd_warning-sign.png" alt="#" /></dt>
+						<dd class="return_warn">
+							<em>교환/ 환불 주의 사항</em>
+							・상품 택과 라벨을 분리하거나 제거하지 마세요<br>
+							・상품 박스를 함께 보내주세요<br>
+							・스타일, 사이즈 확인을 위한 착용을 실내에서 진행해 주세요<br>
+							     <p>—  쉽게 확인할 수 있는 착용 흔적이 있는 경우 교환/환불이 반려될 수 있습니다</p>
+							・사용자 부주의로 인한 상품, 상품박스 파손 시 교환/환불이 반려될 수 있습니다<br>
+						</dd>						
+					</dl>				
+				</div>
+			</div>
+		</article>	
+	</section>
+	<section id="foot_detail">
+		<article>
+			<div class="footer_detail_wrapper">
+				<div class="fd_ship_info">
+					<dl class="fd_grade_badge fd_grade_badge_none">
+						<dt>
+							멤버십에 따른<br> 
+							교환·반품·환불<br>
+							업그레이드
+						</dt>
+						<dd>
+							<span>로그인</span>하고<br>
+							멤버십 등급 혜택을<br>
+							확인하세요
+						</dd>
+					</dl>
+					<div class="fd_ship_info_inner">
+						<h4>배송/교환/환불 안내</h4>
+						<dl>
+							<dt><span class="day">익일</span></dt>
+							<dd class="in_order_info">
+								<p>
+								<em>주문/배송 안내</em>
+								・주문 금액에 상관 없이 무료로 배송해 드립니다<br>
+								・결제한 상품을 <span class="fd_line">익일 출고</span>합니다									
+								</p>
+							</dd>
+							<dt><span class="num">30</span></dt>
+							<dd class="in_change_info">
+								<p>
+									<em>교환 안내</em>
+									・<span class="fd_line">출고 후 30일 이내</span>에 무료로 교환할 수 있습니다<br>
+									・교환 기간 중엔 별도의 교환 배송비가 없습니다 <br>
+									・사이즈 교환, 단순 변심에 의한 교환도 가능합니다
+								</p>
+							</dd>
+							<dt><span class="tester">검수</span></dt>
+							<dd class="in_refund_info">
+								<em>환불 안내</em>
+								<p>
+								・<span class="fd_line">상품 검수 후</span> 환불해 드립니다<br>
+								・반품 시 차감되는 금액 없이 전액 환불해 드립니다<br>
+								・환불 시점<br>
+									<ul>	
+									    <li> — 취소 : 취소 완료 즉시 환불 처리됩니다</li>
+									     <li>— 반품 : 상품 검수 후 환불 처리됩니다</li>
+								     </ul> 
+								・결제 수단 별로 환불 시점이 다릅니다 <br>
+								    <a href="#"> 결제 수단 별 환불 시점 자세히 보기</a> 								
+								</p>
+							</dd>							
+						</dl>
+					</div>
+				</div>
+				<div class="fd_ship_method">
+					<h4>교환/환불 신청 방법</h4>
+					<dl>
+						<dt class="fd_website"><img src="../img/fd_website.png" alt="#" /></dt>
+						<dd class="website">
+							<em>웹사이트</em>
+							・마이페이지 구매내역에서 교환  신청할 수 있습니다<br>
+							<a href="#">구매내역으로 이동</a>							
+						</dd>
+						<dt class="fd_qr"><img src="../img/fd_qr-code.png" alt="#" /></dt>
+						<dd class="qr_code">
+							<em>QR코드 이용</em>
+							・배송 박스에 포함된 QR코드를 이용해 쉽게 교환 신청할 수 있습니다<br>
+							・여러 스마트폰 앱에서 QR코드를 읽을 수 있습니다<br>
+							<p>
+						     —  <a href="#">네이버 앱</a> : 검색창 카메라 아이콘 선택 후 QR-바코드 선택<br>
+						     —  <a href="#">페이스북 앱</a> : 더 보기 아이콘 선택 후 QR코드 선택<br>
+						     </p>							
+						</dd>
+						<dt class="fd_warning"><img src="../img/fd_warning-sign.png" alt="#" /></dt>
+						<dd class="return_warn">
+							<em>교환/ 환불 주의 사항</em>
+							・상품 택과 라벨을 분리하거나 제거하지 마세요<br>
+							・상품 박스를 함께 보내주세요<br>
+							・스타일, 사이즈 확인을 위한 착용을 실내에서 진행해 주세요<br>
+							     <p>—  쉽게 확인할 수 있는 착용 흔적이 있는 경우 교환/환불이 반려될 수 있습니다</p>
+							・사용자 부주의로 인한 상품, 상품박스 파손 시 교환/환불이 반려될 수 있습니다<br>
+						</dd>						
+					</dl>				
+				</div>
+			</div>
+		</article>	
+	</section>
+	<section id="foot_detail">
+		<article>
+			<div class="footer_detail_wrapper">
+				<div class="fd_ship_info">
+					<dl class="fd_grade_badge">
+						<dt><img src="../img/fd_silver-3.png" alt="#" /></dt>
+						<dd>
+							<em>실버III</em>
+							멤버십 혜택
+						</dd>
+					</dl>
+					<div class="fd_ship_info_inner">
+						<h4>배송/교환/환불 안내</h4>
+						<dl>
+							<dt><span class="day">익일</span></dt>
+							<dd class="in_order_info">
+								<p>
+								<em>주문/배송 안내</em>
+								・주문 금액에 상관 없이 무료로 배송해 드립니다<br>
+								・결제한 상품을 <span class="fd_line">익일 출고</span>합니다									
+								</p>
+							</dd>
+							<dt><span class="num">90</span></dt>
+							<dd class="in_change_info">
+								<p>
+									<em>교환 안내</em>
+									・<span class="fd_line">출고 후 90일 이내</span>에 무료로 교환할 수 있습니다<br>
+									・교환 기간 중엔 별도의 교환 배송비가 없습니다 <br>
+									・사이즈 교환, 단순 변심에 의한 교환도 가능합니다
+								</p>
+							</dd>
+							<dt><span class="tester">검수</span></dt>
+							<dd class="in_refund_info">
+								<em>환불 안내</em>
+								<p>
+								・<span class="fd_line">상품 검수 후</span> 환불해 드립니다<br>
+								・반품 시 차감되는 금액 없이 전액 환불해 드립니다<br>
+								・환불 시점<br>
+									<ul>	
+									    <li> — 취소 : 취소 완료 즉시 환불 처리됩니다</li>
+									     <li>— 반품 : 상품 검수 후 환불 처리됩니다</li>
+								     </ul> 
+								・결제 수단 별로 환불 시점이 다릅니다 <br>
+								    <a href="#"> 결제 수단 별 환불 시점 자세히 보기</a> 								
+								</p>
+							</dd>							
+						</dl>
+					</div>
+				</div>
+				<div class="fd_ship_method">
+					<h4>교환/환불 신청 방법</h4>
+					<dl>
+						<dt class="fd_website"><img src="../img/fd_website.png" alt="#" /></dt>
+						<dd class="website">
+							<em>웹사이트</em>
+							・마이페이지 구매내역에서 교환  신청할 수 있습니다<br>
+							<a href="#">구매내역으로 이동</a>							
+						</dd>
+						<dt class="fd_qr"><img src="../img/fd_qr-code.png" alt="#" /></dt>
+						<dd class="qr_code">
+							<em>QR코드 이용</em>
+							・배송 박스에 포함된 QR코드를 이용해 쉽게 교환 신청할 수 있습니다<br>
+							・여러 스마트폰 앱에서 QR코드를 읽을 수 있습니다<br>
+							<p>
+						     —  <a href="#">네이버 앱</a> : 검색창 카메라 아이콘 선택 후 QR-바코드 선택<br>
+						     —  <a href="#">페이스북 앱</a> : 더 보기 아이콘 선택 후 QR코드 선택<br>
+						     </p>							
+						</dd>
+						<dt class="fd_warning"><img src="../img/fd_warning-sign.png" alt="#" /></dt>
+						<dd class="return_warn">
+							<em>교환/ 환불 주의 사항</em>
+							・상품 택과 라벨을 분리하거나 제거하지 마세요<br>
+							・상품 박스를 함께 보내주세요<br>
+							・스타일, 사이즈 확인을 위한 착용을 실내에서 진행해 주세요<br>
+							     <p>—  쉽게 확인할 수 있는 착용 흔적이 있는 경우 교환/환불이 반려될 수 있습니다</p>
+							・사용자 부주의로 인한 상품, 상품박스 파손 시 교환/환불이 반려될 수 있습니다<br>
+						</dd>						
+					</dl>				
+				</div>
+			</div>
+		</article>	
+	</section>		
+	<section id="footInfo">
+    	<div class="custom_inner">
+    		<article class="info">
+        		<h2>Free Shipping & Free Return</h2>
+        		<h3 class="days_count">
+        			<span>30</span>
+        			<span>90</span>
+        			<span>365</span>
+        			<em>DAYS</em>
+        		</h3>
+        		<p>
+        			<strong>멤버십 등급에 따라 길어지는 무료 교환 &middot; 반품기간</strong>
+        			교환 &middot; 반품 배송비가 없는 구루핏에서 당신에게 꼭 맞는 FIT을 쇼핑하세요.
+        		</p>
+        		<a href="#" class="text_more">자세히 보기</a>
+    		</article>
+    		<article class="info">
+        		<h2>Membership</h2>
+        		<ul class="rnd_icon_button">
+        			<li>
+        				<a href="#">
+        					<img src="../img/foo-01.png" alt="">
+        					<span>교환 &middot; 반품<br>무료배송</span>
+        				</a>
+        			</li>
+        			<li>
+        				<a href="#">
+        					<img src="../img/foo-02.png" alt="">
+        					<span>상시<br>할인</span>
+        				</a>
+        			</li>
+        			<li>
+        				<a href="#">
+        					<img src="../img/foo-03.png" alt="">
+        					<span>더 빠른<br>배송</span>
+        				</a>
+        			</li>
+        			<li>
+        				<a href="#">
+        					<img src="../img/foo-04.png" alt="">
+        					<span>프리미엄 &middot;독점 제품<br>선구매 기회</span>
+        				</a>
+        			</li>
+        		</ul>
+        		<p>
+        			<strong>가입만 해도 누구나 멤버쉽 혜택을 누릴 수 있습니다.</strong>
+        			무료 교환 &middot; 반품은 기본! 등급에 따라 더 큰 혜택을 받을 수 있는 멤버쉽 혜택을 만나보세요.
+        		</p>
+        		<a href="#" class="text_more">자세히 보기 </a>
+    		</article>
+		</div>
+	</section>
+	<section id="csInfo">
+		<div class="custom_inner">
+    		<h2>대표전화</h2>
+    		<h3>1544.7777</h3>
+    		<p>1번 : 배송 / 교환 / 환불관련 / 2번 : 결제 / 회원관련 오전9시~오후6시(토,일 공휴일 휴무)</p>
+    		<ul>
+    			<li><a href="#">고객센터</a></li>
+    			<li><a href="#">FAQ</a></li>
+    			<li><a href="#">1:1질문하기</a></li>
+    			<li><a href="#">이메일문의</a></li>
+    		</ul>
+		</div>
+	</section>
+	<nav id="footNav">
+		<div class="custom_inner">
+    		<ul id="navLink">
+    			<li><a href="#">회사소개</a></li>
+    			<li><a href="#">서비스소개</a></li>
+    			<li><a href="#">이용약관</a></li>
+    			<li><a href="#">개인정보취급방침</a></li>
+    			<li><a href="#">청소년 보호정책</a></li>
+    			<li><a href="#">인재채용</a></li>
+    		</ul>
+    		<ul id="snsLink">
+    			<li class="kakao"><a href="#"><img src="../img/kakao-ico.png" alt="KaKao"></a></li>
+    			<li class="facebook"><a href="#"><img src="../img/facebook-ico.png" alt="FaceBook"></a></li>
+    			<li class="twitter"><a href="#"><img src="../img/twitter-ico.png" alt="Twitter"></a></li>
+    			<li class="instagram"><a href="#"><img src="../img/instagram-ico.png" alt="Instagram"></a></li>
+    		</ul>
+		</div>
+	</nav>
+	<address id="address">
+		<div class="custom_inner">
+    		<span>상호명 : 슈포래빗  대표이사 : 안보현</span>
+    		<span>주소 : 서울시 서초구 방배동 901-10 3층</span>
+    		<span>사업자등록번호 : 220-81-62517</span>
+    		<br>
+            <span>통신판매업신고번호 : 제2006-경기성남-692호  <a href="#">사업자등록정보확인</a></span>
+            <br>
+            <span>고객센터 주소 : 서울시 서초구 방배동 901-10 3층</span>
+        	<span>전화 : 1588-3819 (전화전클릭) 1588-3816 (결제도용신고)</span>
+        	<br>
+        	<span>팩스 : 033-816-5300</span>
+        	<span>이메일 : helpcustomer@gurufit.co.kr</span>
+            <p id="copy">COPYRIGHT &copy; GURUFIT ALL RIGHTS RESERVED.</p>
+        </div>
+	</address>
+</footer>
+</div>
+</body>
+</html>

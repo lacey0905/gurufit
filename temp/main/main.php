ba1097eccb63dc10d01b969d06e8b8f9eef44f65
user <user@LAPTOP-5K4FIEFV>
2017-08-29 16:42:31 +0900

    메인

--- temp/main/main.php
+++ temp/main/main.php
@@ -48,30 +48,7 @@
 						<img src="../img/prd_sample.png" alt="샘플상품">
 						<figcaption>
 							<span class="kind">인터뷰</span>
-<<<<<<< HEAD
 							<span class="like">74</span>
-=======
-							<span class="like"><i class="icon-heart"></i> 74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<style>
-	            .prd_type_a figcaption .like {
-				    color:#BBBBBB;	            
-	            }
-			</style>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
 							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
 							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
 							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
@@ -85,11 +62,7 @@
 						<img src="../img/prd_sample.png" alt="샘플상품">
 						<figcaption>
 							<span class="kind">인터뷰</span>
-<<<<<<< HEAD
 							<span class="like">74</span>
-=======
-							<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
 							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
 							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
 							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
@@ -103,11 +76,7 @@
 						<img src="../img/prd_sample.png" alt="샘플상품">
 						<figcaption>
 							<span class="kind">인터뷰</span>
-<<<<<<< HEAD
 							<span class="like">74</span>
-=======
-							<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
 							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
 							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
 							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
@@ -121,11 +90,7 @@
 						<img src="../img/prd_sample.png" alt="샘플상품">
 						<figcaption>
 							<span class="kind">인터뷰</span>
-<<<<<<< HEAD
 							<span class="like">74</span>
-=======
-							<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
 							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
 							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
 							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
@@ -139,67 +104,7 @@
 						<img src="../img/prd_sample.png" alt="샘플상품">
 						<figcaption>
 							<span class="kind">인터뷰</span>
-<<<<<<< HEAD
 							<span class="like">74</span>
-=======
-							<span class="like"><i class="icon-heart"></i> 74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like"><i class="icon-heart"></i> 74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like"><i class="icon-heart"></i> 74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like"><i class="icon-heart"></i> 74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
 							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
 							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
 							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
@@ -220,7 +125,6 @@
 				<a href="#">
 					<figure>
     					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-						<span class="sale">Sale 40%</span>
     					<div class="rating">
     						<span class="sale">Sale 40%</span>
     						<strong class="star_rating">
@@ -231,49 +135,7 @@
     							<i class="star_off"></i>
    							</strong>
     						<span class="cnt">(29)</span>
-<<<<<<< HEAD
     						<span class="like">74</span>
-=======
-    						<span class="like"><i class="icon-heart"></i> 74</span>
-    					</div>
-    					<figcaption>
-    						<span class="subject">NIKE FREE 5.0</span>
-    						<span class="price">
-    							<strong class="sell_price">89,100<em>원</em></strong>
-    							<del class="customer_price">99,000원</del>
-    						</span>
-    					</figcaption>
-					</figure>
-				</a>
-			</li>
-			<style>
-			 .sale {
-                font-size: 11px;
-                font-family: 'texgyreadventorbold_italic';
-                font-weight: 400;
-                color: #f6a623;		 
-			 }
-			 .rating .like {
-			     float:right;
-			     color:#BBBBBB;
-			 }
-			</style>			
-			<li>
-				<a href="#">
-					<figure>
-    					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-    					<span class="sale">Sale 40%</span>
-    					<div class="rating">
-    						<strong class="star_rating">
-    							<i class="star_on"></i>
-    							<i class="star_on"></i>
-    							<i class="star_on"></i>
-    							<i class="star_on"></i>
-    							<i class="star_off"></i>
-   							</strong>
-    						<span class="cnt">(29)</span>
-    						<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
     					</div>
     					<figcaption>
     						<span class="subject">NIKE FREE 5.0</span>
@@ -289,7 +151,6 @@
 				<a href="#">
 					<figure>
     					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-    					<span class="sale">Sale 40%</span>
     					<div class="rating">
     						<span class="sale">Sale 40%</span>
     						<strong class="star_rating">
@@ -300,11 +161,7 @@
     							<i class="star_off"></i>
    							</strong>
     						<span class="cnt">(29)</span>
-<<<<<<< HEAD
     						<span class="like">74</span>
-=======
-    						<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
     					</div>
     					<figcaption>
     						<span class="subject">NIKE FREE 5.0</span>
@@ -320,7 +177,6 @@
 				<a href="#">
 					<figure>
     					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-    					<span class="sale">Sale 40%</span>
     					<div class="rating">
     						<span class="sale">Sale 40%</span>
     						<strong class="star_rating">
@@ -331,11 +187,7 @@
     							<i class="star_off"></i>
    							</strong>
     						<span class="cnt">(29)</span>
-<<<<<<< HEAD
     						<span class="like">74</span>
-=======
-    						<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
     					</div>
     					<figcaption>
     						<span class="subject">NIKE FREE 5.0</span>
@@ -351,7 +203,6 @@
 				<a href="#">
 					<figure>
     					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-    					<span class="sale">Sale 40%</span>
     					<div class="rating">
     						<span class="sale">Sale 40%</span>
     						<strong class="star_rating">
@@ -362,11 +213,7 @@
     							<i class="star_off"></i>
    							</strong>
     						<span class="cnt">(29)</span>
-<<<<<<< HEAD
     						<span class="like">74</span>
-=======
-    						<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
     					</div>
     					<figcaption>
     						<span class="subject">NIKE FREE 5.0</span>
@@ -382,7 +229,6 @@
 				<a href="#">
 					<figure>
     					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-    					<span class="sale">Sale 40%</span>
     					<div class="rating">
     						<span class="sale">Sale 40%</span>
     						<strong class="star_rating">
@@ -393,37 +239,7 @@
     							<i class="star_off"></i>
    							</strong>
     						<span class="cnt">(29)</span>
-<<<<<<< HEAD
     						<span class="like">74</span>
-=======
-    						<span class="like"><i class="icon-heart"></i> 74</span>
-    					</div>
-    					<figcaption>
-    						<span class="subject">NIKE FREE 5.0</span>
-    						<span class="price">
-    							<strong class="sell_price">89,100<em>원</em></strong>
-    							<del class="customer_price">99,000원</del>
-    						</span>
-    					</figcaption>
-					</figure>
-				</a>
-			</li>			
-			<li>
-				<a href="#">
-					<figure>
-    					<span class="thum"><img src="../img/store_sample_255x177.png" alt="샘플상품"></span>
-    					<span class="sale">Sale 40%</span>
-    					<div class="rating">
-    						<strong class="star_rating">
-    							<i class="star_on"></i>
-    							<i class="star_on"></i>
-    							<i class="star_on"></i>
-    							<i class="star_on"></i>
-    							<i class="star_off"></i>
-   							</strong>
-    						<span class="cnt">(29)</span>
-    						<span class="like"><i class="icon-heart"></i> 74</span>
->>>>>>> branch 'master' of https://github.com/lacey0905/gurufit.git
     					</div>
     					<figcaption>
     						<span class="subject">NIKE FREE 5.0</span>
@@ -477,49 +293,7 @@
 						<img src="../img/prd_sample.png" alt="샘플상품">
 						<figcaption>
 							<span class="kind">인터뷰</span>
-							<span class="like">74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like">74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like">74</span>
-							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
-							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
-							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<figure>
-						<img src="../img/prd_sample.png" alt="샘플상품">
-						<figcaption>
-							<span class="kind">인터뷰</span>
-							<span class="like">74</span>
+							<span class="like"><strong class="up">52</strong><strong class="dw">4</strong></span>
 							<span class="subject">나이키랩 줌 플라이 SP 국내 발매 예정일 공개</span>
 							<span class="name_date">By&nbsp;<em>Jin Hong Park</em><i>&nbsp;&middot;&nbsp;</i>May 10.2017</span>
 							<span class="hash">#부스트 #DPR #퓨어부스트 #도심</span>

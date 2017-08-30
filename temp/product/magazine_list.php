1a280192678b0b5ca8326264fa8e02f1523cfb7b
yeseo <yeseo@shoe4rabbit.com>
2017-08-30 15:34:50 +0900

    수정

--- temp/product/magazine_list.php
+++ temp/product/magazine_list.php
@@ -40,8 +40,13 @@
 		</section>
 		<?php include "../inc/mag_side.php" ?>
 		<section class="content_area">
-			<dl class="select_sort">
-				<dt>정렬 기준</dt>
+			<dl class="select_sort select_sort_standard">
+				<dt>
+					<span class="sorting_icon">
+						<i class="fa fa-question-circle" aria-hidden="true"></i>
+						정렬 기준
+					</span>
+				</dt>				
 				<dd>
 					<div class="sel_custom sel_nm_box sel_mid">
 						<select>
@@ -55,6 +60,45 @@
 					</div>
 				</dd>
 			</dl>
+			<div class="mz_sort_pop_wrapper">
+				<div class="mz_sort_pop">
+					<h4>정렬 속성</h4>
+					<dl>
+						<dt>개인 추천 순</dt>
+						<dd>
+						고객 정보(성별,연령,구매내역 등); 사이트 이용 정<br>
+						보(검색,조회,구매,평가,공유 등); 구루핏이 보유하<br>
+						고 있는 '컨텐츠 정보'를 기반으로 고객의 관심사를<br>
+						분석하여 관련성이 높은 콘테느 순으로 정렬됩니다.	
+						</dd>
+						<dt>신규 콘텐츠 순</dt>
+						<dd>
+						최근 등록 된 콘텐츠 순으로 정렬 됩니다.
+						</dd>
+						<dt>신규 콘텐츠 순</dt>
+						<dd>
+						하트를 많이 받은 콘텐츠 순으로 정렬 됩니다.
+						</dd>
+					</dl>
+				</div>
+			</div>
+        	<style>
+        	   .sorting_icon i {
+        	       margin-right:7px;
+        	       font-size:1.3em;
+        	       color:#F0F0F0;
+        	       vertical-align:middle;
+        	   }
+        	   .select_sort_standard {
+        	       position:relative;
+        	   }
+        	   .mz_sort_pop_wrapper {
+        	       position:absolute;
+        	       background-color:red;
+        	       top:0;
+        	       right:0;
+        	   }
+        	</style>			
 			<!-- 진열 영역 -->
 			<article class="custom_prd prd_type_a">
         		<ul class="x3">
--- temp/profile/guru_profile.php
+++ temp/profile/guru_profile.php
@@ -13,22 +13,36 @@
 		<section id="mypage">
 			<div class="guru_profile_info">
 				<h4>언니네 이발소</h4>
-				<div class="guru_line"></div>
-				<ul>
-					<li class="guru_p_tag">
-						<p>
-							슈즈 블로거 언니네이발소가 제안하는 유니크한 자수 디테일이<br>
-							매력적인 자라 청바지 스타일링 데님의 변신은 과연 어디까지<br>
-							기대하시라
-						</p>
-						<em>등록 2017년 8월 12일</em>
-						
-					</li>
-					<li class="guru_p_pho">
-					</li>
-					<li class="guru_p_button">
-					</li>
-				</ul>
+				<div class="guru_p_inner">
+					<ul>
+						<li class="guru_p_tag">
+							<p>
+								슈즈 블로거 언니네이발소가 제안하는 유니크한 자수 디테일이<br>
+								매력적인 자라 청바지 스타일링 데님의 변신은 과연 어디까지<br>
+								기대하시라
+							</p>
+							<em>등록 2017년 8월 12일</em>
+							<ul class="hash">
+								<li><a href="#">#부스트</a></li>
+								<li><a href="#">#아디다스</a></li>
+								<li><a href="#">#도심</a></li>
+								<li><a href="#">#탐험</a></li>
+								<li><a href="#">#탐험</a></li>
+								<li><a href="#">#탐험</a></li>
+							</ul>
+						</li>
+						<li class="guru_p_pho">
+							<img src="../img/guru_profile_photo.png" alt="샘플상품">
+						</li>
+						<li class="guru_p_button">
+							<span>
+								구루를 팔로우 하면 새로운 콘텐츠를<br>
+								가장 빨리 받아볼 수 있습니다.
+							</span>
+							<div class="btn"><a href="#">팔로우</a></div>
+						</li>
+					</ul>
+				</div>
 			</div>
 			<style>
 				.guru_profile_info {
@@ -50,8 +64,92 @@
 					bottom: 0;
 					width: 1px;
 					height: 16px;
-					border-left: solid 1px #979797;					
+					border-left: solid 1px #979797;
 				}
+				.guru_p_inner {
+					margin-bottom:40px;	
+				}
+				.guru_p_inner:after {
+					content:"";
+					display:block;
+					clear:both;
+				}
+				.guru_p_inner ul li {
+					float:left;
+				}
+				.guru_p_tag {
+					width:360px;
+				}
+				.guru_p_tag > p {
+					color: #9B9B9B;	
+					font-size: 13px;	
+					line-height: 18px;
+					margin-top: 67px;					
+				}
+				.guru_p_tag > em {
+					color: #BBBBBB;	
+					font-size: 13px;	
+					line-height: 15px;
+					margin-top:12px;
+					margin-bottom:18px;
+					display:inline-block;					
+				}
+				.guru_p_tag .hash li {
+					display:inline-block;
+					margin: 0 6px 6px 0;
+				}
+				.guru_p_tag .hash li a {
+					display: block;
+					min-width: 52px;
+					font-size: 14px;
+					line-height: 28px;
+					color: #9B9B9B;
+					text-align: center;
+					background-color: #F0F0F0;
+					padding:0 15px;
+					border-radius:5px;
+				}
+				.guru_p_pho {
+					margin-left:48px;
+				}
+				.guru_p_pho img {
+					width:300px;
+					height:300px;
+				}
+				.guru_p_button {
+					margin-top:130px;
+					margin-left:34px;	
+				}
+				.guru_p_button span {
+					display:inline-block;
+					color: #BBBBBB;	
+					font-size: 12px;	
+					line-height: 15px;	
+					text-align: right;
+					margin-right:13px;					
+				}
+				.guru_p_button .btn {
+					display:inline-block;
+					width:140px;
+					border-radius:3px;
+					background-color:#7053FF;
+					text-align:center;					
+				}
+				.guru_p_button .btn:hover {
+					box-shadow: 3px 3px 0 0 #e6e1ff;					
+				}
+				.guru_p_button .btn a {
+					display:block;
+					padding-top:9px;
+					padding-bottom:9px;
+					text-align:center;
+					color: #FFFFFF;		
+					font-size: 15px;	
+					line-height: 22px;	
+					text-align: center;					
+				}
+
+				
 			</style>
 			<div class="tab_cont guru_f_cont">
 				<ul class="tab_info_count">
@@ -189,6 +287,17 @@
 			</div>		
 		</section>
 	</article>
+	<script>
+		// 결제 정보 탭
+   		$(function(){
+   			$(".dash_tab_wrap").sectionDisplay({
+   				act : "click",
+   				start:0,
+   				nav : $('.dash_tab_wrap .tab_nav'),
+   				auto: false
+   			});
+   		});
+	</script>	
 </section>
 <?php include "../inc/footer.php" ?>
  

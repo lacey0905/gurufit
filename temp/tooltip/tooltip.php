2b7480b2a19a636f736d210d77f865b9c58e5b8b
yeseo <yeseo@shoe4rabbit.com>
2017-09-12 16:54:24 +0900

    툴팁부분

--- temp/tooltip/tooltip.php
+++ temp/tooltip/tooltip.php
@@ -241,7 +241,78 @@
 		font-size: 14px;	
 		line-height: 25px;		
 	}
-	</style>		
+	</style>
+	
+	<div class="good_review_w">
+		<div class="good_review_c">
+			<div class="good_review_i">
+				<dl>
+					<dt><img src="../img/g_review.png" alt="이미지">좋은 리뷰를 작성하는 방법</dt>
+					<dd>-리뷰를 작성하기 전,제품을 충분히 사용해 주세요.</dd>
+					<dd class="gr_line">-제품의 기능에 초점을 맞추고 구체적으로 작성해주세요.</dd>	
+					<dt><img src="../img/g_review.png" alt="이미지">적절하지 않은 내용</dt>
+					<dd>-가격,프로모션 정보와 같이 자주 변경되는 정보</dd>
+					<dd>-부적절한 용어 사용</dd>
+					<dd>-다른 회사 및 웹사이트 광고</dd>
+					<dd>-개인정보</dd>
+				</dl>
+			</div>
+		</div>
+	</div>
+	<style>
+		.good_review_w {
+			position:absolute;
+			top:40px; 
+			right:100px;
+		}
+		.good_review_c {
+			width: 320px;	
+			border: 1px solid #DDDDDD;	
+			border-radius: 5px;	
+			background-color: #FFFFFF;	
+			box-shadow: 8px -8px 1px 0 rgba(0,0,0,0.09);
+			padding:21px 14px 13px 19px;		
+		}
+		.good_review_c:after {
+			 content: " ";
+			 position: absolute;
+			 top: 222px;
+			 left: 159px;
+			 display: inline-block;
+			 width: 15px;
+			 height: 15px;
+			 background-color: #fff;
+			 border: solid 1px #DDD;
+			 border-bottom: 0;
+			 border-right: 0;
+			 -ms-transform: rotate(135deg);
+			 -webkit-transform: rotate(135deg);
+			 transform: rotate(-135deg);			
+		}
+		.good_review_i dt {
+			color: #000000;	
+			font-family: "Noto Sans CJK KR";	
+			font-size: 14px;	
+			line-height: 20px;
+			margin-bottom:8px;			
+		}
+		.good_review_i dt img {
+			display:inline-block;
+			width:18px;
+			height:16px;
+			vertical-align:middle;
+			margin-right:11px;
+		}
+		.good_review_i dd {
+			color: #9B9B9B;		
+			font-size: 13px;	
+			line-height: 20px;		
+		}
+		.good_review_i .gr_line {
+			margin-bottom:18px;
+		}
+	</style>
+			
 		
 		
 </div>

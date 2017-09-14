f038280040eb429c3f199806403fc0148b6bbc26
yeseo <yeseo@shoe4rabbit.com>
2017-09-14 17:59:58 +0900

    회원 탈퇴 페이지 완성

---
+++ temp/member/mem_leave.php
@@ -0,0 +1,59 @@
+<?php include "../inc/header.php" ?>
+<section id="member" class="sub_layout">
+	<div class="sub_locate">
+		<div class="custom_inner">
+			<dl class="locate_list">
+				<dt><a href="#">홈</a></dt>
+				<dd><a href="#">로그인</a></dd>
+			</dl>
+			<p class="locate_msg">나이키 플라이니트 최대~50% OFF</p>
+		</div>
+	</div>
+	<article class="sub_content custom_inner">
+		<section class="sub_head">
+			<h2>Member Leave</h2>
+			<div class="tab_active_type">
+				<ul>
+					<li class="active"><a href="#">회원 탈퇴</a></li>
+				</ul>
+			</div>
+		</section>
+		<div class="member_box">
+			<h3>
+			&nbsp;
+			</h3>
+			<div class="complate_box_c">
+				<strong class="comp_tit lev_comp_tit">
+					<span>구루핏 탈퇴가 완료되었습니다.</span>
+					<p>그동안 서비스를 이용해 주셔서 감사합니다.</p>
+				</strong>
+    		</div>
+		</div>
+		<style>
+		.lev_comp_tit {
+			padding:118px 0 124px !important;		
+			color: #000000 !important;	
+			font-size: 30px !important;	
+			line-height: 45px !important;	
+			text-align: center !important;
+			font-weight: 500 !impiortant; 
+		}
+		.lev_comp_tit > span {
+			display:block;
+			color: #000000;	
+			font-size: 30px;	
+			line-height: 45px;	
+			text-align: center;
+			font-weight: 500; 		
+		}
+		.lev_comp_tit > p {
+			color: #000000;	
+			font-size: 20px;	
+			font-weight: 300;	
+			line-height: 29px;	
+			text-align: center;			
+		}	
+		</style>
+	</article>
+</section>
+<?php include "../inc/footer.php" ?>
